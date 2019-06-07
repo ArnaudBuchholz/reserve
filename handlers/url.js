d0151348da89abc9cafa25d29ac3d5a6fc7de1a0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-07 15:33:38 -0400

    url handler

---
+++ handlers/url.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const http = require('http')
+const https = require('https')
+
+function protocol (url) {
+  if (url.startsWith('https')) {
+    return https
+  }
+  return http
+}
+
+function unsecureCookies (headers) {
+  ['Set-Cookie', 'set-cookie'].forEach(name => {
+    if (headers[name]) {
+      headers[name] = headers[name].map(cookie => cookie.replace(/\s*secure;/i, ''))
+    }
+  })
+}
+
+module.exports = {
+  schema: {
+    'unsecure-cookies': 'boolean'
+  },
+  redirect: (request, response) => {
+    const url = request.redirect
+    const {
+      method,
+      headers
+    } = request
+    delete headers.host // Some websites rely on the host header
+    const redirectedRequest = protocol(url).request(url, { method, headers }, redirectedResponse => {
+      if (request.mapping['unsecure-cookies']) {
+        unsecureCookies(redirectedResponse.headers)
+      }
+      response.writeHead(redirectedResponse.statusCode, redirectedResponse.headers)
+      redirectedResponse
+        .on('end', () => log(request, response, redirectedResponse.headers['content-length'] || 0))
+        .pipe(response)
+    })
+    redirectedRequest.on('error', err => error(request, response, { message: err.toString() }))
+    request
+      .on('data', chunk => redirectedRequest.write(chunk))
+      .on('end', () => redirectedRequest.end())
+  }
+}
