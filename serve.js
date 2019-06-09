0a9351f274e7b65417565ab0cd13b696185b0e9d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-09 08:43:05 -0400

    wip

--- serve.js
+++ serve.js
@@ -9,22 +9,6 @@ const readdir = util.promisify(fs.readdir)
 
 const handlers = {}
 
-function buildServer (configuration, requestHandler) {
-  let protocol
-  let server
-  if (configuration.ssl) {
-    protocol = 'https'
-    server = https.createServer({
-      key: fs.readFileSync(configuration.ssl.key),
-      cert: fs.readFileSync(configuration.ssl.cert)
-    }, requestHandler)
-  } else {
-    protocol = 'http'
-    server = http.createServer(requestHandler)
-  }
-  return {server, protocol}
-}
-
 const handlersReady = readdir(path.join(__dirname, 'handlers'))
   .then(names => names
     .filter(name => name.endsWith('.js'))
@@ -33,6 +17,22 @@ const handlersReady = readdir(path.join(__dirname, 'handlers'))
       handlers[name] = require(path.join(__dirname, 'handlers', name + '.js'))
     })
   )
+
+function buildServer (configuration, requestHandler) {
+  let protocol
+  let server
+  if (configuration.ssl) {
+    configuration.protocol = 'https'
+    server = https.createServer({
+      key: fs.readFileSync(configuration.ssl.key),
+      cert: fs.readFileSync(configuration.ssl.cert)
+    }, requestHandler)
+  } else {
+    configuration.protocol = 'http'
+    server = http.createServer(requestHandler)
+  }
+  return server
+}
 
 function requestHandler (request, response) {
   if (this.verbose) {
@@ -53,8 +53,10 @@ function requestHandler (request, response) {
         error(request, response, { message: 'invalid mapping' })
         return false
       }
-      for (let capturingGroupIndex = match.length; capturingGroupIndex > 0; --capturingGroupIndex) {
-        redirect = redirect.replace(new RegExp(`\\$${capturingGroupIndex}`, 'g'), match[capturingGroupIndex])
+      if (typeof redirect == 'string') {
+        for (let capturingGroupIndex = match.length; capturingGroupIndex > 0; --capturingGroupIndex) {
+          redirect = redirect.replace(new RegExp(`\\$${capturingGroupIndex}`, 'g'), match[capturingGroupIndex])
+        }
       }
       if (this.verbose) {
         console.log('SERVE'.magenta, `${request.url} => ${type} ${redirect}`.gray)
@@ -73,7 +75,14 @@ module.exports = configuration => {
   const eventEmitter = new EventEmitter
   handlersReady
     .then(() => validate(configuration))
-    .then(() => )
+    .then(() => new Promise((resolve, reject) => {
+      buildServer(configuration, requestHandler)
+        .listen(configuration.port, configuration.hostname, err => err? reject(err) : resolve())
+    })
+    .then(() => {
+      console.log(`Server running at ${configuration.protocol}://${configuration.hostname}:${configuration.port}/`.yellow)
+      eventEmitter.emit('ready')
+    })
     .catch(reason => eventEmitter.emit('error', reason))
   return eventEmitter
 }
