d318a30fd1defcd6e52ca50d1979b30f2bab320b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-08 23:06:36 -0400

    Promisify handler

--- handlers/file.js
+++ handlers/file.js
@@ -4,20 +4,20 @@ const mime = require('mime')
 
 module.exports = {
   schema: {},
-  redirect: (request, response) => {
+  redirect: (request, response) => new Promise((resolve, reject) => {
     const filePath = /([^?]+)/.exec(request.redirect)[1]
     fs.stat(filePath, (err, stat) => {
       if (err || stat.isDirectory()) {
-        error(request, response, { status: 404, message: 'Not found' })
+        reject(404)
       } else {
         response.writeHead(200, {
           'Content-Type': mime.getType(path.extname(filePath)) || mime.getType('bin'),
           'Content-Length': stat.size
         })
         fs.createReadStream(filePath)
-          .on('end', () => log(request, response, stat.size))
+          .on('end', resolve)
           .pipe(response)
       }
     })
-  }
+  })
 }
