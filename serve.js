5e5a7171f89c534eeb54a354df752ab37479d100
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-10 16:17:25 -0400

    WIP

--- serve.js
+++ serve.js
@@ -1,15 +1,19 @@
 'use strict'
 
+const dispatcher = require('./dispatcher')
+const checkConfiguration = require('./configuration')
 const EventEmitter = require('events')
 const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const path = require('path')
 const util = require('util')
 
-const readdir = util.promisify(fs.readdir)
+const readdirAsync = util.promisify(fs.readdir)
 
 const handlers = {}
 
-const handlersReady = readdir(path.join(__dirname, 'handlers'))
+const handlersReady = readdirAsync(path.join(__dirname, 'handlers'))
   .then(names => names
     .filter(name => name.endsWith('.js'))
     .map(name => path.basename(name, '.js'))
@@ -18,67 +22,26 @@ const handlersReady = readdir(path.join(__dirname, 'handlers'))
     })
   )
 
-function buildServer (configuration, requestHandler) {
-  let protocol
-  let server
+function createServer (configuration, requestHandler) {
   if (configuration.ssl) {
     configuration.protocol = 'https'
-    server = https.createServer({
+    return https.createServer({
       key: fs.readFileSync(configuration.ssl.key),
       cert: fs.readFileSync(configuration.ssl.cert)
     }, requestHandler)
-  } else {
-    configuration.protocol = 'http'
-    server = http.createServer(requestHandler)
   }
-  return server
-}
-
-function requestHandler (request, response) {
-  if (this.verbose) {
-    console.log('SERVE'.magenta, `${request.method} ${request.url}`.gray)
-  }
-  if (this.mappings.every(mapping => {
-    const match = mapping.match.exec(request.url)
-    if (match) {
-      request.mapping = mapping
-      request.match = match
-      let redirect
-      let type
-      if (types.every(member => {
-        type = member
-        redirect = mapping[member]
-        return !redirect
-      })) {
-        error(request, response, { message: 'invalid mapping' })
-        return false
-      }
-      if (typeof redirect == 'string') {
-        for (let capturingGroupIndex = match.length; capturingGroupIndex > 0; --capturingGroupIndex) {
-          redirect = redirect.replace(new RegExp(`\\$${capturingGroupIndex}`, 'g'), match[capturingGroupIndex])
-        }
-      }
-      if (this.verbose) {
-        console.log('SERVE'.magenta, `${request.url} => ${type} ${redirect}`.gray)
-      }
-      typeHandlers[type](request, response)
-        .then(() => { /* document response */}, reason => error(request, response, reason))
-      return false
-    }
-    return true
-  })) {
-    error(request, response, { message: 'not mapped' })
-  }
+  configuration.protocol = 'http'
+  return http.createServer(requestHandler)
 }
 
 module.exports = configuration => {
-  const eventEmitter = new EventEmitter
+  const eventEmitter = new EventEmitter()
   handlersReady
-    .then(() => validate(configuration))
+    .then(() => checkConfiguration(configuration, handlers))
     .then(() => new Promise((resolve, reject) => {
-      buildServer(configuration, requestHandler)
-        .listen(configuration.port, configuration.hostname, err => err? reject(err) : resolve())
-    })
+      createServer(configuration, dispatcher.bind(eventEmitter, configuration))
+        .listen(configuration.port, configuration.hostname, err => err ? reject(err) : resolve())
+    }))
     .then(() => {
       console.log(`Server running at ${configuration.protocol}://${configuration.hostname}:${configuration.port}/`.yellow)
       eventEmitter.emit('ready')
