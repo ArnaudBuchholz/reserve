9a3d5235a378e9eb8dc22896272c2d1c090d9168
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-10 13:02:19 -0400

    Improves promisification Handles index.html for folders

--- handlers/file.js
+++ handlers/file.js
@@ -3,23 +3,35 @@
 const fs = require('fs')
 const mime = require('mime')
 const path = require('path')
+const util = require('util')
+
+const defaultMimeType = mime.getType('bin')
+const statAsync = util.promisify(fs.stat)
+
+function sendFile (response, filePath, stat) {
+  return new Promise((resolve, reject) => {
+    response.writeHead(200, {
+      'Content-Type': mime.getType(path.extname(filePath)) || defaultMimeType,
+      'Content-Length': stat.size
+    })
+    fs.createReadStream(filePath)
+      .on('error', reject)
+      .on('end', resolve)
+      .pipe(response)
+  })
+}
+
+function sendIndex (response, folderPath) {
+  const indexPath = path.join(folderPath, 'index.html')
+  return statAsync(indexPath)
+    .then(stat => sendFile(response, indexPath, stat))
+}
 
 module.exports = {
   schema: {},
-  redirect: (request, response) => new Promise((resolve, reject) => {
-    const filePath = /([^?]+)/.exec(request.redirect)[1]
-    fs.stat(filePath, (err, stat) => {
-      if (err || stat.isDirectory()) {
-        reject(err || new Error('directory'))
-      } else {
-        response.writeHead(200, {
-          'Content-Type': mime.getType(path.extname(filePath)) || mime.getType('bin'),
-          'Content-Length': stat.size
-        })
-        fs.createReadStream(filePath)
-          .on('end', resolve)
-          .pipe(response)
-      }
-    })
+  redirect: (request, response) => {
+    const filePath = /([^?#]+)/.exec(request.redirect)[1] // filter URL parameters & hash
+    return statAsync(filePath)
+      .then(stat => stat.isDirectory() ? sendIndex(response, filePath) : sendFile(response, filePath, stat))
   })
 }
