82364059455a6fe6165112722f0fc905c103560b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-12 14:54:11 -0400

    Handles configuration extension

--- index.js
+++ index.js
@@ -21,9 +21,29 @@ const configurationFileName = process.argv.reduce((name, parameter) => {
   return name
 }, '') || 'reserve.json'
 
+function extend (configurationFilePath, configuration) {
+  if (configuration.extend) {
+    const baseConfigurationFilePath = path.join(path.dirname(configurationFilePath), configuration.extend)
+    delete configuration.extend
+    return readFileAsync(baseConfigurationFilePath)
+      .then(buffer => JSON.parse(buffer.toString()))
+      .then(baseConfiguration => {
+        // Only merge mappings
+        const baseMappings = baseConfiguration.mappings
+        const mergedConfiguration = Object.assign(baseConfiguration, configuration)
+        if (baseMappings !== mergedConfiguration.mappings) {
+          mergedConfiguration.mappings = [...configuration.mappings, baseMappings]
+        }
+        return extend(baseConfigurationFilePath, mergedConfiguration)
+      })
+  }
+  return configuration
+}
+
 const configurationFilePath = path.join(process.cwd(), configurationFileName)
 statAsync(configurationFilePath)
   .then(() => readFileAsync(configurationFilePath).then(buffer => JSON.parse(buffer.toString())))
+  .then(configuration => extend(configurationFilePath, configuration))
   .catch(reason => {
     console.warn(`'${configurationFileName}' not found or invalid, applying defaults`.yellow)
     return {} // empty configuration will use all defaults
