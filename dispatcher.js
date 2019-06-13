233d2c357ca0812b90a42fd2adb1888f26dbf39e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-13 16:19:23 -0400

    WIP

--- dispatcher.js
+++ dispatcher.js
@@ -14,15 +14,11 @@ function error (request, response, message) {
   response.end(content)
 }
 
-module.exports = function (configuration, request, response) {
-  const emitParameters = {
-    method: request.method,
-    url: request.url,
-    start: new Date()
-  }
-  this.emit('incoming', emitParameters)
+function process ({emitParameters, configuration, request, response, url}) {
+  emitParameters.url = url
+  this.emit('processing', emitParameters)
   if (configuration.mappings.every(mapping => {
-    const match = mapping.match.exec(request.url)
+    const match = mapping.match.exec(url)
     if (match) {
       let {
         handler,
@@ -35,22 +31,43 @@ module.exports = function (configuration, request, response) {
         }
       }
       this.emit('redirecting', Object.assign(emitParameters, { type, redirect }))
-      handler.redirect({ mapping, match, redirect, request, response })
-        .then(() => {
-          const end = new Date()
-          this.emit('redirected', Object.assign(emitParameters, {
-            end,
-            timeSpent: end - emitParameters.start,
-            statusCode: response.statusCode
-          }))
-        }, reason => {
-          error(request, response, reason.toString())
-          this.emit('error-redirecting', reason)
-        })
+      try {
+          handler.redirect({ mapping, match, redirect, request, response })
+            .then(() => {
+              const end = new Date()
+              this.emit('redirected', Object.assign(emitParameters, {
+                end,
+                timeSpent: end - emitParameters.start,
+                statusCode: response.statusCode
+              }))
+            }, reason => {
+              error(request, response, reason.toString())
+              this.emit('error', {...emitParameters, reason})
+            })
+      } catch (e) {
+
+      }
       return false
     }
     return true
   })) {
-    error(request, response, { message: 'not mapped' })
+    this.emit('error', {...emitParameters, reason: 'no mapping'})
+    process({emitParameters, configuration, request, response, 500})
   }
+}
+
+module.exports = function (configuration, request, response) {
+  const emitParameters = {
+    method: request.method,
+    url: request.url,
+    start: new Date(),
+    incomingUrl: request.url
+  }
+  this.emit('incoming', emitParameters)
+  process.call(this, {
+    emitParameters,
+    configuration,
+    request,
+    response
+  })
 }
