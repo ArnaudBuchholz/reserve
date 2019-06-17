374de2ad1c7cd93984c8f2e43874719a5d334887
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-17 12:36:27 -0400

    run all tests

--- tests/all.js
+++ tests/all.js
@@ -1,3 +1,7 @@
+'use strict'
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
 require('colors')
 global.gpf = require('gpf-js')
 global.assert = require('assert')
@@ -9,10 +13,12 @@ const assertions = require('./assertions')
 const path = require('path')
 const { fork } = require('child_process')
 
-function startServer () {
+function startServer (cmdLineParameters) {
   console.log('Starting reserve'.cyan)
+  console.log(`directory:  ${process.cwd()}`.gray)
+  console.log(`parameters: ${cmdLineParameters.join(' ')}`.gray)
   return new Promise(resolve => {
-    const server = fork(path.join(__dirname, '../index.js'), [].slice.call(arguments))
+    const server = fork(path.join(__dirname, '../index.js'), cmdLineParameters)
     server.on('message', message => {
       console.log('reserve message'.yellow, message.gray)
       if (message === 'ready') {
@@ -22,29 +28,30 @@ function startServer () {
   })
 }
 
-function stopServer (server) {
-  server.kill('SIGINT')
+async function testServer (origin, ...cmdLineParameters) {
+  const server = await startServer(cmdLineParameters)
+  location.origin = origin
+  try {
+    await assertions()
+  } finally {
+    server.kill('SIGINT')
+  }
 }
 
 async function all () {
   const now = new Date()
-  let failed = false
-  const server1 = await startServer('--config', 'http')
-  location.origin = 'http://localhost:5000'
   try {
-    await assertions()
+    process.chdir('./tests')
+    await testServer('http://localhost:5000', '--config', 'http.json')
+    await testServer('https://localhost:5001', '--config', 'https.json')
+    process.chdir('./sub')
+    await testServer('http://localhost:5002')
+    console.error(`All tests succeeded`.green)
   } catch (e) {
     failed = true
-    console.error(e.toString())
-  }
-  stopServer(server1)
-  console.log('Total time (ms): ', new Date() - now)
-  if (failed) {
-    console.error(`At least one test failed`.red)
-    process.exit(-1)
-  } else {
-    console.error(`All tests succeeded`.green)
-    process.exit(0)
+    console.error(e.toString().red)
+  } finally {
+    console.log('Total time (ms): ', new Date() - now)
   }
 }
 
