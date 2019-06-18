ba0086f2b3c180f568bcdc6fe90a0fc65e6b734d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-18 15:59:00 -0400

    status handler

---
+++ handlers/status.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const mime = require('mime')
+const textMimeType = mime.getType('text')
+const byStatus = {
+  '403': 'Forbidden',
+  '404': 'Not found',
+  '500': 'Internal Server Error'
+}
+
+module.exports = {
+  schema: {
+    status: 'number'
+  },
+  redirect: async function ({ response, redirect }) {
+    const statusCode = redirect
+    const content = byStatus[statusCode] || ''
+    const length = content.length
+    response.writeHead(statusCode, {
+      'Content-Type': textMimeType,
+      'Content-Length': length
+    })
+    response.end(content)
+  }
+}
