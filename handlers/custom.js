dcf59f1e15f61921b7a80a47a2aa658efac05a56
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-19 16:13:01 -0400

    custom handler tested

--- handlers/custom.js
+++ handlers/custom.js
@@ -4,14 +4,19 @@ module.exports = {
   schema: {
     custom: 'function'
   },
-  redirect: ({ mapping, match, request, response }) => new Promise((resolve, reject) => {
+  redirect: ({ mapping, match, request, response }) => new Promise(resolve => {
     // Include timeout?
     const parameters = [request, response].concat([].slice.call(match, 1))
-    const result = mapping.custom.apply(mapping, parameters)
-    if (result && typeof result.then === 'function') {
-      result.then(resolve, () => 500)
-    } else {
-      resolve()
+    try {
+      const result = mapping.custom.apply(mapping, parameters)
+      if (result && typeof result.then === 'function') {
+        result.then(resolve, () => resolve(500))
+      } else {
+        resolve()
+      }
+    } catch (e) {
+      // TODO document issue
+      resolve(500)
     }
   })
 }
