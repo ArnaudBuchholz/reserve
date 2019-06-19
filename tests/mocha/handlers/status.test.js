9e2106f3d47b3937fb18c7574bb17788a89a2454
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-19 13:24:41 -0400

    testing status handler

---
+++ tests/mocha/handlers/status.test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const assert = require('assert')
+const mime = require('mime')
+const Response = require('../Response')
+const statusHandler = require('../../../handlers/status')
+
+const textMimeType = mime.getType('text')
+
+describe('handlers/status', () => {
+  it('returns a promise', () => {
+    const response = new Response()
+    const result = statusHandler.redirect({ response, redirect: 404 })
+    assert(typeof result.then === 'function')
+  })
+
+  it('sends a generic response for knonw codes', () => {
+    const response = new Response()
+    return statusHandler.redirect({ response, redirect: 404 })
+      .then(() => {
+        assert(response.statusCode === 404)
+        assert(response.headers['Content-Type'] === textMimeType)
+        assert(response.toString() === 'Not found')
+      })
+  })
+
+  it('sends an empty response for unknonw codes', () => {
+    const response = new Response()
+    return statusHandler.redirect({ response, redirect: 418 })
+      .then(() => {
+        assert(response.statusCode === 418)
+        assert(response.headers['Content-Type'] === textMimeType)
+        assert(response.toString() === '')
+      })
+  })
+})
