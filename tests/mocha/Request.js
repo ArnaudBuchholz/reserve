9de726625c17adaccec6ff164fee609212e2844b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-20 08:17:34 -0400

    Improves request simulation

--- tests/mocha/Request.js
+++ tests/mocha/Request.js
@@ -1,5 +1,19 @@
 'use strict'
 
-module.exports = class Request {
+const Readable = require('stream').Readable
 
+module.exports = class Request extends Readable {
+  constructor (method = 'GET', url = '/') {
+    super()
+    this._method = method
+    this._url = url
+  }
+
+  get method () {
+    return this._method
+  }
+
+  get url () {
+    return this._url
+  }
 }
