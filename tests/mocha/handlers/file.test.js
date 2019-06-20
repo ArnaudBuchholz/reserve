9b1a0eae9b1e79ad185acf86e063f863be2bbb5e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-20 13:30:22 -0400

    Testing file handler

--- tests/mocha/handlers/file.test.js
+++ tests/mocha/handlers/file.test.js
@@ -1,15 +1,21 @@
 'use strict'
 
-require('../fs-mock')
+require('../mocked_modules/path')
+require('../mocked_modules/fs')
 
 const assert = require('../assert')
+const mime = require('mime')
 const Request = require('../Request')
 const Response = require('../Response')
 const fileHandler = require('../../../handlers/file')
 
+const textMimeType = mime.getType('text')
+const htmlMimeType = mime.getType('html')
+const defaultMimeType = mime.getType('bin')
+
 describe('handlers/file', () => {
   it('returns a promise', () => {
-    const request = new Request('GET', '/file')
+    const request = new Request()
     const response = new Response()
     const result = fileHandler.redirect({
       request,
@@ -17,8 +23,133 @@ describe('handlers/file', () => {
       mapping: {
         _path: '/'
       },
-      redirect: 'file'
+      redirect: './file.txt'
     })
     assert(() => typeof result.then === 'function')
   })
+
+  it('pipes file content (relative path)', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './file.txt'
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === textMimeType)
+        assert(() => response.toString() === 'Hello World!')
+      })
+  })
+
+  it('pipes file content (absolute path)', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/folder'
+      },
+      redirect: '/file.txt'
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === textMimeType)
+        assert(() => response.toString() === 'Hello World!')
+      })
+  })
+
+  it('sends index.html if accessing a folder', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './folder'
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === htmlMimeType)
+        assert(() => response.toString() === '<html />')
+      })
+  })
+
+  it('defaults mimetype when no extension', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './file'
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === defaultMimeType)
+        assert(() => response.toString() === 'binary')
+      })
+  })
+
+  it('fails with 404 if the file does not exist', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './not-found'
+    })
+      .then(value => {
+        assert(() => value === 404)
+      })
+  })
+
+  it('fails with 403 if the folder does not have index.html', () => {
+    const request = new Request()
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './no-index'
+    })
+      .then(value => {
+        assert(() => value === 403)
+      })
+  })
+
+  const methods = ['POST', 'PUT', 'DELETE']
+  methods.forEach(method => it(`fails with 405 for ${method}`, () => {
+    const request = new Request(method)
+    const response = new Response()
+    return fileHandler.redirect({
+      request,
+      response,
+      mapping: {
+        _path: '/'
+      },
+      redirect: './file.txt'
+    })
+      .then(value => {
+        assert(() => value === 405)
+      })
+  }))
 })
