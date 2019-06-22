d76d4f6681211c90bd701e1a133838448f6c2fb1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-22 13:35:19 -0400

    echo simulation

--- tests/mocha/http.js
+++ tests/mocha/http.js
@@ -3,17 +3,25 @@
 const EventEmitter = require('events')
 const Response = require('./Response')
 const empty = 'http://www.mocked.com/empty'
+const echo = 'http://www.mocked.com/empty'
 
 module.exports = {
 
   urls: {
-    empty
+    empty,
+    echo
   },
 
   request: (url, options, callback) => {
     const eventEmitter = new EventEmitter()
-    const response = new Response(200)
+    const response = new Response()
     if (url === empty) {
+      response.writeHead(200)
+    } else if (url === echo) {
+      const statusCode = options.headers['x-status-code']
+      response.writeHead(statusCode, options.headers)
+    }
+    eventEmitter.end = () => {
       callback(response)
     }
     return eventEmitter
