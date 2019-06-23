90b4d89097b039c36389b1f7c2f6a4ebace3791c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-22 23:35:41 -0400

    Test url handler

--- tests/mocha/handlers/url.test.js
+++ tests/mocha/handlers/url.test.js
@@ -16,10 +16,70 @@ describe('handlers/url', () => {
     const result = urlHandler.redirect({
       request,
       response,
-      mapping: {
-      },
+      mapping: {},
       redirect: http.urls.empty
     })
     assert(() => typeof result.then === 'function')
+  })
+
+  it('pipes URL content', () => {
+    const request = new Request('GET', 'http://example.com/abwhatever', {
+      'x-status-code': 200,
+      'x-value-1': 'test',
+      'host': 'http://example.com'
+    }, 'Hello World!')
+    const response = new Response()
+    return urlHandler.redirect({
+      request,
+      response,
+      mapping: {},
+      redirect: http.urls.echo
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['x-value-1'] === 'test')
+        assert(() => response.headers['host'] === undefined)
+        assert(() => response.toString() === 'Hello World!')
+      })
+  })
+
+  it('pipes URL content (https)', () => {
+    const request = new Request('GET', 'http://example.com/abwhatever', {
+      'x-status-code': 200
+    }, 'Hello World!')
+    const response = new Response()
+    return urlHandler.redirect({
+      request,
+      response,
+      mapping: {},
+      redirect: http.urls.echos
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.toString() === 'Hello World!')
+      })
+  })
+
+  it('unsecures cookies', () => {
+    const request = new Request('GET', 'http://example.com/abwhatever', {
+      'x-status-code': 200,
+      'Set-Cookie': ['name=value; Secure']
+    })
+    const response = new Response()
+    return urlHandler.redirect({
+      request,
+      response,
+      mapping: {
+        'unsecure-cookies': true
+      },
+      redirect: http.urls.echos
+    })
+      .then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Set-Cookie'] === 'name=value;')
+      })
   })
 })
