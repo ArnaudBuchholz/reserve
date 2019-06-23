5b3ce9fe1f2a866f08d2ccc1eeb79c4ea6cee9d4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-22 23:35:18 -0400

    handles pipe correctly

--- tests/mocha/http.js
+++ tests/mocha/http.js
@@ -4,27 +4,33 @@ const EventEmitter = require('events')
 const Response = require('./Response')
 const empty = 'http://www.mocked.com/empty'
 const echo = 'http://www.mocked.com/echo'
+const echos = 'https://www.mocked.com/echo'
 
 module.exports = {
 
   urls: {
     empty,
-    echo
+    echo,
+    echos
   },
 
   request: (url, options, callback) => {
-    const eventEmitter = new EventEmitter()
+    const result = new EventEmitter()
     const response = new Response()
     if (url === empty) {
       response.writeHead(200)
-    } else if (url === echo) {
+    } else if (url === echo || url === echos) {
       const statusCode = options.headers['x-status-code']
       response.writeHead(statusCode, options.headers)
     }
-    eventEmitter.end = () => {
+    result.write = chunk => {
+      response.write(chunk)
+    }
+    result.end = () => {
+      response.end()
       callback(response)
     }
-    return eventEmitter
+    return result
   }
 
 }
