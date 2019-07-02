52e7c32b9357aeaab80016fe36ab4b9a2160ed4c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-02 12:41:42 -0400

    Simplifies custom handler by letting error / rejection flows

--- handlers/custom.js
+++ handlers/custom.js
@@ -4,19 +4,13 @@ module.exports = {
   schema: {
     custom: 'function'
   },
-  redirect: ({ mapping, match, request, response }) => new Promise(resolve => {
+  redirect: ({ mapping, match, request, response }) => {
     // Include timeout?
     const parameters = [request, response].concat([].slice.call(match, 1))
-    try {
-      const result = mapping.custom.apply(mapping, parameters)
-      if (result && typeof result.then === 'function') {
-        result.then(resolve, () => resolve(500))
-      } else {
-        resolve()
-      }
-    } catch (e) {
-      // TODO document issue
-      resolve(500)
+    const result = mapping.custom.apply(mapping, parameters)
+    if (result && typeof result.then === 'function') {
+      return result
     }
-  })
+    return Promise.resolve()
+  }
 }
