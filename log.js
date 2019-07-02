41df1a6c9c037752f3d66bfff5d6a207a76f8133
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-02 16:21:18 -0400

    Make default logs reusable

---
+++ log.js
@@ -0,0 +1,40 @@
+'use strict'
+
+module.exports = (serve, verbose) => {
+  serve
+    .on('ready', ({ url }) => {
+      console.log(`Server running at ${url}`.yellow)
+      if (process.send) {
+        process.send('ready')
+      }
+    })
+    .on('error', ({ method, url, reason }) => {
+      if (method && url) {
+        console.error('ERROR'.red, method.gray, url.gray, '\n\\____'.red, reason.toString().gray)
+      } else {
+        console.error('ERROR'.red, reason.toString().gray)
+      }
+    })
+    .on('redirected', ({ method, url, statusCode, timeSpent }) => {
+      let report
+      if (statusCode > 399) {
+        report = statusCode.toString().red
+      } else {
+        report = statusCode.toString().green
+      }
+      report += ` ${timeSpent} ms`.magenta
+      console.log('SERVE'.magenta, method.gray, url.gray, report)
+    })
+
+  if (verbose) {
+    serve.on('redirecting', ({ method, url, type, redirect }) => {
+      let redirectLabel
+      if (typeof redirect === 'function') {
+        redirectLabel = '(function)'
+      } else {
+        redirectLabel = redirect.toString()
+      }
+      console.log('RDRCT'.gray, method.gray, url.gray, '\n\\____'.gray, type.gray, redirectLabel.gray)
+    })
+  }
+}
