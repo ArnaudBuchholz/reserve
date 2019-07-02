8a9568c821e3d1da9d4c15d28414416ed4574075
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-02 16:40:05 -0400

    Smaller index (won't be unit tested)

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 'use strict'
 
-/* global process */
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
-
-require('colors')
-const util = require('util')
-const serve = require('./serve')
-const fs = require('fs')
-const path = require('path')
+const readConfiguration = require('./configuration').read
 const log = require('./log')
-
-const statAsync = util.promisify(fs.stat)
-const readFileAsync = util.promisify(fs.readFile)
+const serve = require('./serve')
 
 const configurationFileName = process.argv.reduce((name, parameter) => {
   if (parameter === '--config') {
@@ -23,51 +14,9 @@ const configurationFileName = process.argv.reduce((name, parameter) => {
   return name
 }, '') || 'reserve.json'
 
-function extend (configurationFilePath, configuration) {
-  const configurationFolderPath = path.dirname(configurationFilePath)
-  if (configuration.mappings) {
-    configuration.mappings.forEach(mapping => {
-      if (!mapping._path) {
-        mapping._path = configurationFolderPath
-      }
-    })
-  }
-  if (configuration.extend) {
-    const baseConfigurationFilePath = path.join(configurationFolderPath, configuration.extend)
-    delete configuration.extend
-    return readFileAsync(baseConfigurationFilePath)
-      .then(buffer => JSON.parse(buffer.toString()))
-      .then(baseConfiguration => {
-        // Only merge mappings
-        const baseMappings = baseConfiguration.mappings
-        const mergedConfiguration = Object.assign(baseConfiguration, configuration)
-        if (baseMappings !== mergedConfiguration.mappings) {
-          mergedConfiguration.mappings = [...configuration.mappings, baseMappings]
-        }
-        return extend(baseConfigurationFilePath, mergedConfiguration)
-      })
-  }
-  return configuration
-}
-
-let configurationFilePath
-if (path.isAbsolute(configurationFileName)) {
-  configurationFilePath = configurationFileName
-} else {
-  configurationFilePath = path.join(process.cwd(), configurationFileName)
-}
-statAsync(configurationFilePath)
-  .then(() => readFileAsync(configurationFilePath).then(buffer => JSON.parse(buffer.toString())))
-  .then(configuration => extend(configurationFilePath, configuration))
+readConfiguration(configurationFileName)
   .catch(reason => {
     console.warn(`'${configurationFileName}' not found or invalid, applying defaults`.yellow)
     return {} // empty configuration will use all defaults
   })
-  .then(configuration => {
-    if (process.argv.includes('--verbose')) {
-      configuration.verbose = true
-    }
-    configuration._json = true
-    return configuration
-  })
-  .then(configuration => log(serve(configuration), configuration.verbose))
+  .then(configuration => log(serve(configuration), process.argv.includes('--verbose')))
