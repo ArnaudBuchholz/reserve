14bfec9b2bf19616f000d359e9709ca84b8d2761
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-03 13:19:26 -0400

    Testing configuration basics

--- tests/mocha/configuration.test.js
+++ tests/mocha/configuration.test.js
@@ -3,6 +3,12 @@
 const assert = require('./assert')
 const { read, check } = require('../../configuration')
 
+const shouldFail = promise => promise.then(() => {
+  assert(() => false) // Not expected
+}, () => {
+  assert(() => true) // expected
+})
+
 describe('configuration', () => {
   describe('configuration.read', () => {
     process.mockCwd('/')
@@ -18,7 +24,7 @@ describe('configuration', () => {
         })
     })
 
-    it('reads a configuration and extends it', () => {
+    it('reads a configuration and extends it (also documents cwd)', () => {
       return read('/folder/reserve.json')
         .then(configuration => {
           assert(() => configuration.port === 3475)
@@ -34,23 +40,25 @@ describe('configuration', () => {
         })
     })
 
-    const shouldFail = promise => promise.then(() => {
-      assert(() => false) // Not expected
-    }, () => {
-      assert(() => true) // expected
+    it('reads a configuration and extends it (relative path)', () => {
+      return read('/folder/reserve-with-another-port.json')
+        .then(configuration => {
+          assert(() => configuration.port === 220103)
+          assert(() => configuration.ssl !== undefined)
+          assert(() => configuration.mappings.length === 2)
+          assert(() => configuration.mappings[0].match === '/folder/.*')
+          assert(() => configuration.mappings[0].url === 'https://secured.com/$1')
+          assert(() => configuration.mappings[0].cwd === '/folder')
+          // Extended mappings have a lower priority
+          assert(() => configuration.mappings[1].match === '/(.*)')
+          assert(() => configuration.mappings[1].file === '/$1')
+          assert(() => configuration.mappings[1].cwd === '/')
+        })
     })
 
-    it('handles error (invalid extend)', () => {
-      return shouldFail(read('/folder/invalid-extend.json'))
-    })
-
-    it('handles error (invalid json)', () => {
-      return shouldFail(read('/folder/index.html'))
-    })
-
-    it('handles error (no file)', () => {
-      return shouldFail(read('/folder/not-found.json'))
-    })
+    it('handles error (invalid extend)', () => shouldFail(read('/folder/invalid-extend.json')))
+    it('handles error (invalid json)', () => shouldFail(read('/folder/index.html')))
+    it('handles error (no file)', () => shouldFail(read('/folder/not-found.json')))
   })
 
   describe('configuration.check', () => {
@@ -70,6 +78,68 @@ describe('configuration', () => {
             assert(() => initialConfiguration !== checkedConfiguration)
           })
         )
+    })
+
+    it('loads ssl info', () => {
+      return read('/folder/reserve.json')
+        .then(check)
+        .then(configuration => {
+          assert(() => configuration.ssl.key === 'privatekey')
+          assert(() => configuration.ssl.cert === 'certificate')
+        })
+    })
+
+    it('validates handlers used', () => shouldFail(check({
+      mappings: [{
+        match: '(.*)',
+        invalid: '$1'
+      }]
+    })))
+
+    it('loads custom handlers using require', () => {
+      return check({
+        mappings: [{
+          match: '(.*)',
+          cwd: __dirname,
+          custom: 'custom.js'
+        }]
+      })
+        .then(configuration => {
+          assert(() => typeof configuration.mappings[0].custom === 'function')
+          return configuration.mappings[0].custom()
+        })
+    })
+
+    it('prevents overriding of the default handlers', () => {
+      return check({
+        handlers: {
+          file: {
+            overridden: true
+          }
+        }
+      })
+        .then(configuration => {
+          assert(() => typeof configuration.handlers.file.custom === 'undefined')
+          assert(() => typeof configuration.handlers.file.redirect === 'function')
+        })
+    })
+
+    it('allows custom handlers', () => {
+        return check({
+          handlers: {
+            mock: {
+              redirect: () => Promise.resolve()
+            }
+          },
+          mappings: [{
+            match: /(.*)/,
+            mock: '$1'
+          }]
+        })
+        .then(configuration => {
+          assert(() => typeof configuration.handlers.mock.redirect === 'function')
+          assert(() => Object.keys(configuration.handlers).length > 4)
+        })
     })
   })
 })
