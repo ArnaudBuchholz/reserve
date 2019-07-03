d205128714bca4867398a9c698fa25b1826fbeb9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-03 09:57:53 -0400

    Handlers are now under the responsibility of configuration

--- serve.js
+++ serve.js
@@ -1,26 +1,10 @@
 'use strict'
 
-const checkConfiguration = require('./configuration').check
+const { check } = require('./configuration')
 const dispatcher = require('./dispatcher')
 const EventEmitter = require('events')
-const fs = require('fs')
 const http = require('http')
 const https = require('https')
-const path = require('path')
-const util = require('util')
-
-const readdirAsync = util.promisify(fs.readdir)
-
-const handlers = {}
-
-const handlersReady = readdirAsync(path.join(__dirname, 'handlers'))
-  .then(names => names
-    .filter(name => name.endsWith('.js'))
-    .map(name => path.basename(name, '.js'))
-    .forEach(name => {
-      handlers[name] = require(path.join(__dirname, 'handlers', name + '.js'))
-    })
-  )
 
 function createServer (configuration, requestHandler) {
   if (configuration.ssl) {
@@ -41,8 +25,7 @@ function createServerAsync (configuration, requestHandler) {
 
 module.exports = jsonConfiguration => {
   const eventEmitter = new EventEmitter()
-  handlersReady
-    .then(() => checkConfiguration(jsonConfiguration, handlers))
+  check(jsonConfiguration)
     .then(configuration => createServerAsync(configuration, dispatcher.bind(eventEmitter, configuration))
       .then(() => {
         eventEmitter.emit('ready', {
