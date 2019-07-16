3281e7f44fa20d584a6e54f3257f6d9991768d78
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-16 08:12:32 -0400

    Document error in verbose mode

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ const configurationFileName = process.argv.reduce((name, parameter) => {
   return name
 }, '') || 'reserve.json'
 
+const verbose = process.argv.includes('--verbose')
+
 read(configurationFileName)
   .catch(reason => {
+    if (verbose) {
+      console.error(reason.toString().red)
+    }
     console.warn(`'${configurationFileName}' not found or invalid, applying defaults`.yellow)
     return {} // empty configuration will use all defaults
   })
   .then(configuration => {
-    log(serve(configuration), process.argv.includes('--verbose'))
+    log(serve(configuration), verbose)
       .on('ready', () => {
         if (process.send) {
           process.send('ready')
