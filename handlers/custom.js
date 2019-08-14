f0a9a2517870d6f0004463893561f40789667b6c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-14 13:38:12 -0400

    Implement reload on timestamp change

--- handlers/custom.js
+++ handlers/custom.js
@@ -1,16 +1,40 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const statAsync = util.promisify(fs.stat)
+
 module.exports = {
   schema: {
-    custom: 'function'
+    custom: ['function', 'string'],
+    watch: {
+      type: 'boolean',
+      defaultValue: false
+    }
   },
-  redirect: ({ mapping, match, request, response }) => {
+  validate: async mapping => {
+    if (typeof mapping.custom === 'string') {
+      mapping._path = path.join(mapping.cwd, mapping.custom)
+      mapping._callback = require(mapping._path)
+      if (mapping.watch) {
+        mapping._mtime = (await statAsync(mapping._path)).mtime
+      }
+    } else {
+      mapping._callback = mapping.custom;
+    }
+  },
+  redirect: async ({ mapping, match, request, response }) => {
+    if (mapping._mtime) {
+      const mtime = (await statAsync(mapping._path)).mtime
+      if (mtime !== mapping._mtime) {
+        mapping._mtime = mtime
+        delete require.cache[mapping._path]
+        mapping._callback = require(mapping._path)
+      }
+    }
     // Include timeout?
     const parameters = [request, response].concat([].slice.call(match, 1))
-    const result = mapping.custom.apply(mapping, parameters)
-    if (result && typeof result.then === 'function') {
-      return result
-    }
-    return Promise.resolve()
+    return mapping._callback.apply(mapping, parameters)
   }
 }
