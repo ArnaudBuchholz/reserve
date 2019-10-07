9815bae7664dca965b8e19855d9a76b38245d20d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-07 13:17:08 -0400

    Simple mock handler (#1)

---
+++ mock.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { check } = require('./configuration')
+const dispatcher = require('./dispatcher')
+const EventEmitter = require('events')
+const Request = require('./tests/mocha/Request')
+const Response = require('./tests/mocha/Response')
+
+module.exports = jsonConfiguration => {
+  const eventEmitter = new EventEmitter()
+  check(jsonConfiguration)
+    .then(configuration => {
+      const dispatch = dispatcher.bind(eventEmitter, configuration)
+      eventEmitter.request = (method = 'GET', url = '/', headers = {}, body = '') => {
+        const request = new Request(method, url, headers, body)
+        const response = new Response()
+        return dispatch(request, response).then(() => response)
+      }
+      eventEmitter.emit('ready', {
+        url: `${configuration.protocol}://${configuration.hostname}:${configuration.port}/`
+      })
+    })
+    .catch(reason => eventEmitter.emit('error', { reason }))
+  return eventEmitter
+}
