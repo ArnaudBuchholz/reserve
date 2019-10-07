07967d2af99243e19177c3387dfe768ede6168d5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-07 13:31:14 -0400

    Allow mocking of handlers (#1)

--- mock.js
+++ mock.js
@@ -6,10 +6,11 @@ const EventEmitter = require('events')
 const Request = require('./tests/mocha/Request')
 const Response = require('./tests/mocha/Response')
 
-module.exports = jsonConfiguration => {
+module.exports = (jsonConfiguration, mockedHandlers = {}) => {
   const eventEmitter = new EventEmitter()
   check(jsonConfiguration)
     .then(configuration => {
+      Object.assign(configuration.handlers, mockedHandlers)
       const dispatch = dispatcher.bind(eventEmitter, configuration)
       eventEmitter.request = (method = 'GET', url = '/', headers = {}, body = '') => {
         const request = new Request(method, url, headers, body)
