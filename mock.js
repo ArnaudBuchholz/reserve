8d7174dee2dee000232ae3745ea35a9b163fb08f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-08 08:57:00 -0400

    Simplify usage (#1)

--- mock.js
+++ mock.js
@@ -8,19 +8,15 @@ const Response = require('./tests/mocha/Response')
 
 module.exports = (jsonConfiguration, mockedHandlers = {}) => {
   const eventEmitter = new EventEmitter()
-  check(jsonConfiguration)
+  return check(jsonConfiguration)
     .then(configuration => {
       Object.assign(configuration.handlers, mockedHandlers)
       const dispatch = dispatcher.bind(eventEmitter, configuration)
-      eventEmitter.request = (method = 'GET', url = '/', headers = {}, body = '') => {
+      eventEmitter.request = (method, url, headers = {}, body = '') => {
         const request = new Request(method, url, headers, body)
         const response = new Response()
         return dispatch(request, response).then(() => response)
       }
-      eventEmitter.emit('ready', {
-        url: `${configuration.protocol}://${configuration.hostname}:${configuration.port}/`
-      })
+      return eventEmitter
     })
-    .catch(reason => eventEmitter.emit('error', { reason }))
-  return eventEmitter
 }
