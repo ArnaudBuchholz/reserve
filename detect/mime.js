70905a2fed4a4c9d71575be0869350478c92edfd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-10 23:58:11 -0400

    mime detection and simulation (#4)

---
+++ detect/mime.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let mime
+
+try {
+  mime = require('mime')
+} catch (e) {}
+
+const app = 'application'
+const html = 'text/html'
+const jpeg = 'image/jpeg'
+const text = `${app}/text`
+
+const types = {
+  bin: `${app}/octet-stream`,
+  css: 'text/css',
+  gif: 'image/gif',
+  html,
+  htm: html,
+  jpeg,
+  jpg: jpeg,
+  js: `${app}/javascript`,
+  pdf: `${app}/pdf`,
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  text,
+  txt: text,
+  xml: `${app}/xml`
+}
+
+module.exports = mime || {
+  getType: extension => {
+    if (extension.charAt(0) === '.') {
+      extension = extension.substring(1)
+    }
+    return types[extension] || types.bin
+  }
+}
