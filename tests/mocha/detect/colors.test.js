0ceb47eb85505bc28f3eb22e27455fa5c968e3b7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-11 12:49:44 -0400

    Testing simplified modules (#4)

---
+++ tests/mocha/detect/colors.test.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const assert = require('../assert')
+const mockRequire = require('mock-require')
+
+function cleanRequireCache () {
+  Object.keys(require.cache).forEach(path => {
+    if (path.match(/(node_modules|detect)(\/|\\)colors/)) {
+      delete require.cache[path]
+    }
+  })
+}
+
+describe('detect/colors', () => {
+  let colors
+
+  before(() => {
+    cleanRequireCache()
+    mockRequire('colors/safe', null)
+    colors = require('../../../detect/colors')
+  })
+
+  after(() => {
+    mockRequire.stop('colors/safe')
+    cleanRequireCache()
+  })
+
+  'red,green,cyan,magenta,white,gray'
+    .split(',')
+    .forEach(method => {
+      it(`returns the initial string when using colors.${method}`, () => {
+        assert(() => colors[method]('test') === 'test')
+      })
+    })
+})
