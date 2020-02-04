ac094801e8f6ccb518c5f8058c858f1650d71c2c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-02-04 12:31:08 -0500

    Read-only members

--- iconfiguration.js
+++ iconfiguration.js
@@ -6,4 +6,12 @@ module.exports = class IConfiguration {
   constructor (configuration) {
     this[$configuration] = configuration
   }
+
+  get handlers () {
+    return Object.assign({}, this[$configuration].handlers)
+  }
+
+  get mappings () {
+    return [].concat(this[$configuration].mappings)
+  }
 }
