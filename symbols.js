a2b4114ea48932dacbe9b981d5d3354daa31238b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-02-09 23:01:32 -0500

    $mappingChecked

--- symbols.js
+++ symbols.js
@@ -6,6 +6,7 @@ module.exports = {
   $configuration: Symbol(`${ns}configuration`),
   $configurationInterface: Symbol(`${ns}configuration.interface`),
   $configurationRequests: Symbol(`${ns}configuration.requests`),
+  $mappingChecked: Symbol(`${ns}mapping.checked`),
   $requestPromise: Symbol(`${ns}request.promise`),
   $requestRedirectCount: Symbol(`${ns}request.redirectCount`),
   $responseEnded: Symbol(`${ns}response.ended`)
