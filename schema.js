a7b014bcae4e99901ab1a4a7e9e6d67187ad5d24
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-02-11 23:05:59 -0500

    Ideas for schema validation

--- schema.js
+++ schema.js
@@ -1,7 +1,24 @@
 'use strict'
 
+function validate (property, object, value = property.defaultValue) {
+  const { name, types } = property
+  if (value === undefined) {
+    throw new Error(`Missing property ${name}`)
+  }
+  const valueType = typeof value
+  if (!types.includes(valueType)) {
+    throw new Error(`Invalid type of property ${name}`)
+  }
+  object[property.name] = value
+}
 
-module.exports = (schemaDefinition, object) => {
-  const schema = read(schemaDefinition)
+function parseProperty (name, value) {
+  if (typeof value === 'string') {
+    return { name, types: [ value ]}
+  }
+}
 
+module.exports = (schema, object) => {
+  Object.keys(schema).map(name => parseProperty(name, schema[name]))
+    .forEach(property => validate(property, object, object[property.name]))
 }
