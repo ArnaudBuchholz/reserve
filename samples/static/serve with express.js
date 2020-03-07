65c2e99054d8ede70d0ac2bc87e17e50e873ac29
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-06 23:20:19 -0500

    static web site with express

---
+++ samples/static/serve with express.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 8081
+const wwwRoot = path.join(__dirname, '../www')
+app.use(express.static(wwwRoot))
+app.get('/', (req, res) => res.sendFile(path.join(wwwRoot, 'static.html')))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
