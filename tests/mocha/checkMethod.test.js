371a621e0b2be146519d54151bccc91ccf8bd1dd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-12 12:57:25 -0400

    Fully test checkMethod

--- tests/mocha/checkMethod.test.js
+++ tests/mocha/checkMethod.test.js
@@ -3,10 +3,20 @@
 const assert = require('./assert')
 const checkMethod = require('../../checkMethod')
 
-function test (method, expected) {
+function test (method, expected, allowed) {
   return () => {
+    if (allowed) {
+        debugger
+    }
     const object = { method }
-    checkMethod(object, 'checked')
+    try {
+      checkMethod(object, 'checked', allowed)
+    } catch (e) {
+      /* istanbul ignore else */ // That would be unexpected
+      if (expected === Error) {
+        return // Absorb error
+      }
+    }
     const result = object.checked
     if (expected) {
       assert(() => result.length === expected.length)
@@ -18,7 +28,19 @@ function test (method, expected) {
 }
 
 describe('checkMethod', () => {
-  it('ignores undefined method', test(undefined, undefined))
-  it('validates string method', test('post', ['POST']))
-  it('validates string methods', test('get,post', ['GET', 'POST']))
+  describe('value validation', () => {
+    it('ignores undefined', test(undefined, undefined))
+    it('validates string', test('post', ['POST']))
+    it('validates comma separated string', test('get,post', ['GET', 'POST']))
+    it('validates array', test(['get' , 'post'], ['GET', 'POST']))
+    it('invalidates function', test(test, Error))
+    const invalidValues = [[], false, true, 0, 1, Symbol()]
+    invalidValues.forEach(value => it(`invalidates ${JSON.stringify(value)}`, test(value, Error)))
+  })
+
+  describe('combine with allowed methods', () => {
+    it('keeps allowed methods', test('get,post', ['GET', 'POST'], ['GET', 'POST']))
+    it('filters out unallowed', test('get,post', ['GET'], ['GET']))
+    it('fails if no more method left', test('post', Error, ['GET']))
+  })
 })
