422ff564542fa610084ba2a13985306b0ba929d1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-12 12:57:14 -0400

    Fails if not method left

--- checkMethod.js
+++ checkMethod.js
@@ -2,7 +2,7 @@
 
 module.exports = (object, checkedMember, allowed) => {
   let method = object.method
-  if (!method) {
+  if (method === undefined) {
     object[checkedMember] = allowed
     return
   }
@@ -14,7 +14,10 @@ module.exports = (object, checkedMember, allowed) => {
   }
   method = method.map(verb => verb.toUpperCase())
   if (allowed) {
-    method = method.filter(verb => allowed.includes(method))
+    method = method.filter(verb => allowed.includes(verb))
+  }
+  if (!method.length) {
+    throw new Error('No method specified (or left)')
   }
   object[checkedMember] = method
 }
