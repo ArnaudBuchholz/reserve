472a98d76f32b700a1686a321cb3153c43045e02
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-20 08:50:09 -0400

    Enables object interpolation

--- interpolate.js
+++ interpolate.js
@@ -1,14 +1,29 @@
 'use strict'
 
-module.exports = function (match, value) {
-  if (typeof value === 'string') {
-    return value.replace(/\$(\d+|\$)/g, (token, sIndex) => {
-      if (sIndex === '$') {
-        return '$'
-      } else {
-        return match[sIndex] || ''
-      }
+const types = {
+  string: (match, value) => value.replace(/\$(\d+|\$)/g, (token, sIndex) => {
+    if (sIndex === '$') {
+      return '$'
+    } else {
+      return match[sIndex] || ''
+    }
+  }),
+
+  object: (match, object) => {
+    const interpolated = {}
+    Object.keys(object).forEach(key => {
+      interpolated[key] = interpolate(match, object[key])
     })
+    return interpolated
+  }
+}
+
+function interpolate (match, value) {
+  const byType = types[typeof value]
+  if (byType) {
+    return byType(match, value)
   }
   return value
 }
+
+module.exports = interpolate
