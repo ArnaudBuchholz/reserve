1b0e5e84d6f39beb33a66e107712bfd73ec533cf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-20 10:42:44 -0400

    Implementing headers option

--- handlers/status.js
+++ handlers/status.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const mime = require('../detect/mime')
+const interpolate = require('../interpolate')
 const textMimeType = mime.getType('text')
 const byStatus = {
   403: 'Forbidden',
@@ -12,13 +13,22 @@ const byStatus = {
 }
 
 module.exports = {
-  redirect: async function ({ response, redirect }) {
+  schema: {
+    status: 'number',
+    headers: {
+      type: 'object',
+      defaultValue: {}
+    }
+  },
+  redirect: async function ({ mapping, match, response, redirect }) {
     const statusCode = redirect
     const content = byStatus[statusCode] || ''
     const length = content.length
+    const headers = mapping ? mapping.headers : undefined
     response.writeHead(statusCode, {
       'Content-Type': textMimeType,
-      'Content-Length': length
+      'Content-Length': length,
+      ...interpolate(match, headers)
     })
     response.end(content)
   }
