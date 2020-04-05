90304d970b200fa620d27cbda8f2f79300060e19
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-04-05 13:53:35 -0400

    Use redirect to benefit from caching

--- samples/openui5/proxy-version.js
+++ samples/openui5/proxy-version.js
@@ -1,5 +1,8 @@
 module.exports = async function (request, response, ui5Path) {
   const { referer } = request.headers
   const version = (/\bversion\b=(\d+\.\d+\.\d+)/.exec(referer) || [0, '1.76.0'])[1]
-  return `/@openui5/${version}/${ui5Path}`
+  response.writeHead(302, {
+    Location: `/@openui5/${version}/${ui5Path}`
+  })
+  response.end()
 }
