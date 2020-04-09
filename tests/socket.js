fd7eaf6db0895ae9807668c63f2a91f3ade598df
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-04-08 21:28:31 -0400

    socket example

---
+++ tests/socket.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path')
+const { serve } = require('..')
+serve({
+  port: 8081,
+  plugins: [{
+    alterHttpServer: function (server) {
+      this.io = require('socket.io')(server)
+    }
+  }],
+  mappings: [{
+    match: /^\/(.*)/,
+    file: path.join(__dirname, '$1')
+  }]
+})
+  .on('ready', ({ url }) => {
+      console.log(`Server running at ${url}`)
+  })
