64d16a4f8f143b3c4d8260895c1bbcec46e6e089
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-04-11 23:47:59 -0400

    Validating listeners list

--- tests/mocha/configuration.test.js
+++ tests/mocha/configuration.test.js
@@ -72,6 +72,7 @@ describe('configuration', () => {
           assert(() => typeof configuration.port === 'number')
           assert(() => configuration.hostname === undefined)
           assert(() => configuration.mappings.length === 2)
+          assert(() => configuration.listeners.length === 0)
         })
     })
 
@@ -93,177 +94,225 @@ describe('configuration', () => {
         })
     })
 
-    it('validates handlers used', () => shouldFail(check({
-      mappings: [{
-        match: '(.*)',
-        invalid: '$1'
-      }]
-    })))
-
-    it('allows optional match', () => check({
-      mappings: [{
-        file: '$1'
-      }]
-    })
-      .then(configuration => {
-        assert(() => configuration.mappings[0].match instanceof RegExp)
-      })
-    )
-
-    const invalidMatches = [
-      true,
-      false,
-      0,
-      1,
-      {}
-    ]
-
-    invalidMatches.forEach(invalidMatch => {
-      it(`validates match (${JSON.stringify(invalidMatch)})`, () => shouldFail(check({
+    describe('mappings', () => {
+      it('validates handlers used', () => shouldFail(check({
         mappings: [{
-          match: invalidMatch,
+          match: '(.*)',
+          invalid: '$1'
+        }]
+      })))
+
+      it('allows optional match', () => check({
+        mappings: [{
+          file: '$1'
+        }]
+      })
+        .then(configuration => {
+          assert(() => configuration.mappings[0].match instanceof RegExp)
+        })
+      )
+
+      const invalidMatches = [
+        true,
+        false,
+        0,
+        1,
+        {}
+      ]
+
+      invalidMatches.forEach(invalidMatch => {
+        it(`validates match (${JSON.stringify(invalidMatch)})`, () => shouldFail(check({
+          mappings: [{
+            match: invalidMatch,
+            invalid: '$1'
+          }]
+        })))
+      })
+
+      it('validates match (function)', () => shouldFail(check({
+        mappings: [{
+          match: shouldFail,
+          invalid: '$1'
+        }]
+      })))
+
+      it('validates match (Symbol)', () => shouldFail(check({
+        mappings: [{
+          match: Symbol('test'),
           invalid: '$1'
         }]
       })))
     })
 
-    it('validates match (function)', () => shouldFail(check({
-      mappings: [{
-        match: shouldFail,
-        invalid: '$1'
-      }]
-    })))
-
-    it('validates match (Symbol)', () => shouldFail(check({
-      mappings: [{
-        match: Symbol('test'),
-        invalid: '$1'
-      }]
-    })))
-
-    it('loads custom handlers using require', () => {
-      return check({
-        mappings: [{
-          match: '(.*)',
-          cwd: __dirname,
-          custom: 'custom.js'
-        }]
-      })
-        .then(configuration => {
-          assert(() => typeof configuration.mappings[0][$customCallback] === 'function')
-          return configuration.mappings[0][$customCallback]()
+    describe('handlers', () => {
+      it('loads custom handlers using require', () => {
+        return check({
+          mappings: [{
+            match: '(.*)',
+            cwd: __dirname,
+            custom: 'custom.js'
+          }]
         })
-    })
+          .then(configuration => {
+            assert(() => typeof configuration.mappings[0][$customCallback] === 'function')
+            return configuration.mappings[0][$customCallback]()
+          })
+      })
 
-    it('prevents overriding of the default handlers', () => {
-      return check({
-        handlers: {
-          file: {
-            overridden: true
+      it('prevents overriding of the default handlers', () => {
+        return check({
+          handlers: {
+            file: {
+              overridden: true
+            }
           }
-        }
-      })
-        .then(configuration => {
-          assert(() => typeof configuration.handlers.file.custom === 'undefined')
-          assert(() => typeof configuration.handlers.file.redirect === 'function')
         })
-    })
+          .then(configuration => {
+            assert(() => typeof configuration.handlers.file.custom === 'undefined')
+            assert(() => typeof configuration.handlers.file.redirect === 'function')
+          })
+      })
 
-    it('allows custom handlers', () => {
-      return check({
+      it('allows custom handlers', () => {
+        return check({
+          handlers: {
+            mock: {
+              redirect: okHandler
+            }
+          },
+          mappings: [{
+            match: /(.*)/,
+            mock: '$1'
+          }]
+        })
+          .then(configuration => {
+            assert(() => Object.keys(configuration.handlers).length > 4)
+            assert(() => typeof configuration.handlers.mock.redirect === 'function')
+            return configuration.handlers.mock.redirect()
+          })
+          .then(value => assert(() => value === 'OK'))
+      })
+
+      it('validates custom handlers', () => shouldFail(check({
         handlers: {
           mock: {
-            redirect: okHandler
+            reidrect: okHandler
           }
         },
         mappings: [{
           match: /(.*)/,
           mock: '$1'
         }]
-      })
-        .then(configuration => {
-          assert(() => Object.keys(configuration.handlers).length > 4)
-          assert(() => typeof configuration.handlers.mock.redirect === 'function')
-          return configuration.handlers.mock.redirect()
-        })
-        .then(value => assert(() => value === 'OK'))
-    })
+      })))
 
-    it('validates custom handlers', () => shouldFail(check({
-      handlers: {
-        mock: {
+      it('allows injecting handlers through require', () => {
+        const mockedHandler = {
+          redirect: okHandler
+        }
+        require('mock-require')('mocked-handler', mockedHandler)
+        return check({
+          handlers: {
+            mock: 'mocked-handler'
+          },
+          mappings: [{
+            match: /(.*)/,
+            mock: '$1'
+          }]
+        })
+          .then(configuration => {
+            assert(() => Object.keys(configuration.handlers).length > 4)
+            assert(() => typeof configuration.handlers.mock.redirect === 'function')
+            return configuration.handlers.mock.redirect()
+          })
+          .then(value => assert(() => value === 'OK'))
+      })
+
+      it('validates injected handlers', () => {
+        const mockedHandler = {
           reidrect: okHandler
         }
-      },
-      mappings: [{
-        match: /(.*)/,
-        mock: '$1'
-      }]
-    })))
-
-    it('allows injecting handlers through require', () => {
-      const mockedHandler = {
-        redirect: okHandler
-      }
-      require('mock-require')('mocked-handler', mockedHandler)
-      return check({
-        handlers: {
-          mock: 'mocked-handler'
-        },
-        mappings: [{
-          match: /(.*)/,
-          mock: '$1'
-        }]
+        require('mock-require')('invalid-handler', mockedHandler)
+        return shouldFail(check({
+          handlers: {
+            mock: 'invalid-handler'
+          },
+          mappings: [{
+            match: /(.*)/,
+            mock: '$1'
+          }]
+        }))
       })
-        .then(configuration => {
-          assert(() => Object.keys(configuration.handlers).length > 4)
-          assert(() => typeof configuration.handlers.mock.redirect === 'function')
-          return configuration.handlers.mock.redirect()
-        })
-        .then(value => assert(() => value === 'OK'))
+
+      it('injects handlers relatively to the reserve.json file (same level)', () => {
+        const mockedHandler = {
+          redirect: okHandler
+        }
+        require('mock-require')('/folder/mocked-relative-handler.js', mockedHandler)
+        return read('/folder/reserve-relative-handler.json')
+          .then(check)
+      })
+
+      it('injects handlers relatively to the reserve.json file (parent)', () => {
+        const mockedHandler = {
+          redirect: okHandler
+        }
+        require('mock-require')('/mocked-parent-handler.js', mockedHandler)
+        return read('/folder/reserve-parent-handler.json')
+          .then(check)
+      })
+
+      it('injects absolute handlers', () => {
+        const mockedHandler = {
+          redirect: okHandler
+        }
+        require('mock-require')('mocked-absolute-handler', mockedHandler)
+        return read('/folder/reserve-absolute-handler.json')
+          .then(check)
+      })
     })
 
-    it('validates injected handlers', () => {
-      const mockedHandler = {
-        reidrect: okHandler
-      }
-      require('mock-require')('invalid-handler', mockedHandler)
-      return shouldFail(check({
-        handlers: {
-          mock: 'invalid-handler'
-        },
-        mappings: [{
-          match: /(.*)/,
-          mock: '$1'
-        }]
-      }))
-    })
+    describe('listeners', () => {
+      const invalidListenersList = [
+        true,
+        false,
+        0,
+        1,
+        {},
+        ''
+      ]
 
-    it('injects handlers relatively to the reserve.json file (same level)', () => {
-      const mockedHandler = {
-        redirect: okHandler
-      }
-      require('mock-require')('/folder/mocked-relative-handler.js', mockedHandler)
-      return read('/folder/reserve-relative-handler.json')
-        .then(check)
-    })
+      invalidListenersList.forEach(invalidListeners => {
+        it(`validates listeners (${JSON.stringify(invalidListeners)})`, () => shouldFail(check({
+          listeners: invalidListeners
+        })))
+      })
 
-    it('injects handlers relatively to the reserve.json file (parent)', () => {
-      const mockedHandler = {
-        redirect: okHandler
-      }
-      require('mock-require')('/mocked-parent-handler.js', mockedHandler)
-      return read('/folder/reserve-parent-handler.json')
-        .then(check)
-    })
+      it('validates listeners (function)', () => shouldFail(check({
+        listeners: shouldFail
+      })))
 
-    it('injects absolute handlers', () => {
-      const mockedHandler = {
-        redirect: okHandler
-      }
-      require('mock-require')('mocked-absolute-handler', mockedHandler)
-      return read('/folder/reserve-absolute-handler.json')
-        .then(check)
+      it('validates listeners (Symbol)', () => shouldFail(check({
+        listeners: Symbol('test')
+      })))
+
+      const invalidListenerValues = [
+        true,
+        false,
+        0,
+        1,
+        {},
+        ''
+      ]
+
+      invalidListenerValues.forEach(invalidListener => {
+        it(`validates listener values (${JSON.stringify(invalidListener)})`, () => shouldFail(check({
+          listeners: [invalidListener]
+        })))
+      })
+
+      it('validates listener values (Symbol)', () => shouldFail(check({
+        listeners: [Symbol('test')]
+      })))
     })
   })
 })
