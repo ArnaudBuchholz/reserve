5939acfaf6c3486c27f9647e5b825d22bf2a0463
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-05-16 09:53:30 -0400

    Self sufficient sample

--- tests/cache.js
+++ tests/cache.js
@@ -9,15 +9,29 @@ const mkdirAsync = require('util').promisify(mkdir)
 const cacheBasePath = join(__dirname, 'cache')
 mkdirAsync(cacheBasePath, { recursive: true }) // Should wait for completion
 
-module.exports = async (request, response) => {
-  if (/\.(js|css|svg|jpg)$/.exec(request.url)) {
-    const cachePath = join(cacheBasePath, '.' + request.url)
-    const cacheFolder = dirname(cachePath)
-    await mkdirAsync(cacheFolder, { recursive: true })
-    const file = createWriteStream(cachePath)
-    capture(response, file)
-      .catch(reason => {
-        console.error(`Unable to cache ${cachePath}`, reason)
-      })
-  }
-}
+const { log, serve } = require('..')
+log(serve({
+  port: 8005,
+  mappings: [{
+    match: /^\/(.*)/,
+    file: './cache/$1',
+    'ignore-if-not-found': true
+  }, {
+    method: 'GET',
+    custom: async (request, response) => {
+      if (/\.(js|css|svg|jpg)$/.exec(request.url)) {
+        const cachePath = join(cacheBasePath, '.' + request.url)
+        const cacheFolder = dirname(cachePath)
+        await mkdirAsync(cacheFolder, { recursive: true })
+        const file = createWriteStream(cachePath) // auto closed
+        capture(response, file)
+          .catch(reason => {
+            console.error(`Unable to cache ${cachePath}`, reason)
+          })
+      }
+    }
+  }, {
+    match: /^\/(.*)/,
+    url: 'http://facetheforce.today/$1'
+  }]
+}), process.argv.includes('--verbose'))
