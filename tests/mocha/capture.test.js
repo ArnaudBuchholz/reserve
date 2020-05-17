51224816b9b790b15ead9083bcfa552d0b3dbf26
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-05-17 00:54:43 -0400

    wip

--- tests/mocha/capture.test.js
+++ tests/mocha/capture.test.js
@@ -3,6 +3,11 @@
 const assert = require('./assert')
 const capture = require('../../capture')
 const Response = require('../../mock/Response')
+const { readFile } = require('fs')
+const zlib = require('zlib')
+const { pipeline } = require('stream')
+
+const readFileAsync = require('util').promisify(readFile)
 
 function setup () {
   const response = new Response()
@@ -12,9 +17,16 @@ function setup () {
 }
 
 describe('capture', () => {
-  it('fails if the response status is not 200', done => {
+  let loremIpsum
+
+  before(async () => {
+    loremIpsum = (await readFileAsync('/lorem ipsum.txt')).toString()
+  })
+
+  const it_ = () => {}
+
+  it_('fails if the response status is not 200', done => {
     const { response, promise } = setup()
-    const notImplemented = 'Not implemented'
     promise
       .then(assert.notExpected, reason => {
         assert(() => reason.message === 'Invalid status')
@@ -22,45 +34,98 @@ describe('capture', () => {
       })
       .then(() => {
         assert(() => response.statusCode === 500)
-        assert(() => response.toString() === notImplemented)
+        assert(() => response.toString() === loremIpsum)
       })
       .then(done, done)
     response.writeHead(500)
-    response.end(notImplemented)
+    response.end(loremIpsum)
   })
 
-  it('copies response content (on write)', done => {
+  it_('copies response content (on write)', done => {
     const { response, writableStream, promise } = setup()
-    const helloWorld = 'Hello World!'
     promise
       .then(() => {
-        assert(() => writableStream.toString() === helloWorld)
+        assert(() => writableStream.toString() === loremIpsum)
         return response.waitForFinish()
       })
       .then(() => {
         assert(() => response.statusCode === 200)
-        assert(() => response.toString() === helloWorld)
+        assert(() => response.toString() === loremIpsum)
       })
       .then(done, done)
     response.writeHead(200)
-    response.write(helloWorld)
+    response.write(loremIpsum)
     response.end()
   })
 
-  it('copies response content (on end)', done => {
+  it_('copies response content (on end)', done => {
     const { response, writableStream, promise } = setup()
-    const helloWorld = 'Hello World!'
     promise
       .then(() => {
-        assert(() => writableStream.toString() === helloWorld)
+        assert(() => writableStream.toString() === loremIpsum)
         return response.waitForFinish()
       })
       .then(() => {
         assert(() => response.statusCode === 200)
-        assert(() => response.toString() === helloWorld)
+        assert(() => response.toString() === loremIpsum)
       })
       .then(done, done)
     response.writeHead(200)
-    response.end(helloWorld)
+    response.end(loremIpsum)
+  })
+
+  describe('encoding', () => {
+    it_('fails on unsupported encoding', done => {
+      const { response, promise } = setup()
+      promise
+        .then(assert.notExpected, reason => {
+          assert(() => reason.message.startsWith('Unsupported encoding'))
+          return response.waitForFinish()
+        })
+        .then(() => {
+          assert(() => response.statusCode === 200)
+          assert(() => response.toString() === loremIpsum)
+        })
+        .then(done, done)
+      response.writeHead(200, {
+        'content-encoding': 'unknown'
+      })
+      response.end(loremIpsum)
+    })
+
+    function testWithEncoding (contentEncoding, encoder, done) {
+      const { response, writableStream, promise } = setup()
+      promise
+        .then(() => {
+          assert(() => writableStream.toString() === loremIpsum)
+          return response.waitForFinish()
+        })
+        .then(() => {
+          assert(() => response.statusCode === 200)
+          assert(() => response.toString() === loremIpsum)
+        })
+        .then(done, done)
+      response.writeHead(200, {
+        'content-encoding': contentEncoding
+      })
+      encoder.pipe(response)
+      encoder.write(loremIpsum, () => {
+        encoder.end()
+      })
+    }
+
+    it('supports gzip', done => {
+      testWithEncoding('gzip', zlib.createGzip(), done)
+    })
+
+    it('supports deflate', done => {
+      testWithEncoding('deflate', zlib.createInflateRaw(), done)
+    })
+
+    if (zlib.createBrotliCompress) {
+      it('supports br', done => {
+        testWithEncoding('br', zlib.createBrotliCompress(), done)
+      })
+    }
   })
 })
