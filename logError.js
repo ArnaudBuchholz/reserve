27dedeb5c525246bdbfe4cbaec9cc53ee9fa36e6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-03 00:18:28 -0400

    Rely on common handler

--- logError.js
+++ logError.js
@@ -1,12 +1,13 @@
 'use strict'
 
-const colors = require('./detect/colors')
+const { gray, red } = require('./detect/colors')
+const logCommon = require('./logCommon')
 
-module.exports = ({ method, url, reason }) => {
-  if (method && url) {
-    console.error(colors.red('ERROR'), colors.gray(method), colors.gray(url), colors.red('\n\\____'),
-      colors.gray(reason.toString()))
+module.exports = event => {
+  const reason = event.reason
+  if (event.method && event.url) {
+    logCommon.call(event, 'ERROR', false, red('\n\\____'), gray(reason.toString()))
   } else {
-    console.error(colors.red('ERROR'), colors.gray(reason.toString()))
+    logCommon.call({ ...event, method: '', url: '' }, 'ERROR', false, gray(reason.toString()))
   }
 }
