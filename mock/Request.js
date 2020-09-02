cbc0d5ee879382fd38b7a3aeadea3b938cc04589
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-01 23:11:23 -0400

    Supports several signatures

--- mock/Request.js
+++ mock/Request.js
@@ -8,12 +8,35 @@ module.exports = class Request extends Readable {
     this.push(null)
   }
 
-  constructor (method = 'GET', url = '/', headers = {}, body = '') {
-    super()
+  _fromObject ({ method, url, headers = {}, body = '', properties }) {
     this._method = method
     this._url = url
     this._headers = headers
     this._body = body
+    if (properties) {
+      Object.assign(this, properties)
+    }
+  }
+  
+  _fromParams (method, url, headers, body, properties) {
+    if (typeof body === 'object') {
+      properties = body
+      body = ''
+    }
+    if (typeof headers === 'string') {
+      body = headers
+      headers = {}
+    }
+    return this._fromObject({ method, url, headers, body, properties })
+  }
+
+  constructor (param) {
+    super()
+    if (typeof param === 'object') {
+      this._fromObject(param)
+    } else {
+      this._fromParams(...arguments)
+    }
   }
 
   get method () {
