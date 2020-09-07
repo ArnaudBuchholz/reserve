a5eddf761d7a705ab5d31c98b8685a044fdd82f8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-07 00:56:09 -0400

    Custom file system (WIP)

--- handlers/file.js
+++ handlers/file.js
@@ -1,15 +1,19 @@
 'use strict'
 
 const fs = require('fs')
+const util = require('util')
 const mime = require('../detect/mime')
 const path = require('path')
-const util = require('util')
 
 const defaultMimeType = mime.getType('bin')
-const statAsync = util.promisify(fs.stat)
-const readdirAsync = util.promisify(fs.readdir)
 
-function sendFile (request, response, filePath, stat) {
+const nodeFs = {
+  stat: util.promisify(fs.stat),
+  readdir: util.promisify(fs.readdir),
+  createReadStream: (path, options) => Promise.resolve(fs.createReadStream(path, options))
+}
+
+function sendFile ({ request, response, fs, filePath }, stat) {
   return new Promise((resolve, reject) => {
     response.writeHead(200, {
       'Content-Type': mime.getType(path.extname(filePath)) || defaultMimeType,
@@ -21,29 +25,39 @@ function sendFile (request, response, filePath, stat) {
     } else {
       response.on('finish', resolve)
       fs.createReadStream(filePath)
-        .on('error', reject)
-        .pipe(response)
+        .then(stream => {
+          stream
+            .on('error', reject)
+            .pipe(response)
+        })
     }
   })
 }
 
-function sendIndex (request, response, folderPath) {
-  const indexPath = path.join(folderPath, 'index.html')
-  return statAsync(indexPath)
-    .then(stat => sendFile(request, response, indexPath, stat))
+function sendIndex (context) {
+  const filePath = path.join(context.filePath, 'index.html')
+  return context.fs.stat(filePath)
+    .then(stat => {
+      if (stat.isDirectory()) {
+        throw new Error('index.html not a file')
+      }
+      return sendFile({ ...context, filePath }, stat)
+    })
 }
 
-async function checkCaseSensitivePath (filePath) {
+async function checkCaseSensitivePath (fs, filePath) {
   const folderPath = path.dirname(filePath)
   if (folderPath && folderPath !== filePath) {
     const name = path.basename(filePath)
-    const names = await readdirAsync(folderPath)
+    const names = await fs.readdir(folderPath)
     if (!names.includes(name)) {
       throw new Error('Not found')
     }
-    return checkCaseSensitivePath(folderPath)
+    return checkCaseSensitivePath(fs, folderPath)
   }
 }
+
+const cfs = 'custom-file-system'
 
 module.exports = {
   schema: {
@@ -51,12 +65,29 @@ module.exports = {
       type: 'boolean',
       defaultValue: false
     },
+    [cfs]: {
+      type: 'object',
+      defaultValue: null
+    },
     'ignore-if-not-found': {
       type: 'boolean',
       defaultValue: false
     }
   },
   method: 'GET,HEAD',
+  validate: async mapping => {
+    if (!mapping[cfs]) {
+      mapping[cfs] = nodeFs
+    } else {
+      if (typeof mapping[cfs] === 'string') {
+        mapping[cfs] = require(mapping[cfs])
+      }
+      const types = [typeof mapping[cfs].stat, typeof mapping[cfs].readdir, typeof mapping[cfs].createReadStream]
+      if (types.some(type => type !== 'function')) {
+        throw new Error('Invalid custom-file-system specification')
+      }
+    }
+  },
   redirect: ({ request, mapping, redirect, response }) => {
     let filePath = /([^?#]+)/.exec(unescape(redirect))[1] // filter URL parameters & hash
     if (!path.isAbsolute(filePath)) {
@@ -66,19 +97,25 @@ module.exports = {
     if (directoryAccess) {
       filePath = filePath.substring(0, filePath.length - 1)
     }
-    return statAsync(filePath)
+    const context = {
+      request,
+      response,
+      fs: mapping[cfs],
+      filePath
+    }
+    return context.fs.stat(filePath)
       .then(async stat => {
         if (mapping['case-sensitive']) {
-          await checkCaseSensitivePath(filePath)
+          await checkCaseSensitivePath(context.fs, filePath)
         }
         const isDirectory = stat.isDirectory()
         if (isDirectory ^ directoryAccess) {
           return 404 // Can't ignore if not found
         }
         if (isDirectory) {
-          return sendIndex(request, response, filePath)
+          return sendIndex(context)
         }
-        return sendFile(request, response, filePath, stat)
+        return sendFile(context, stat)
       })
       .catch(() => {
         if (!mapping['ignore-if-not-found']) {
