a2b4c8e74b3f96741d93f84b80942c8f920a636f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-08 23:41:36 -0400

    Test handlers

---
+++ tests/mocha/handlers/handle.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Request = require('../../../mock/Request')
+const Response = require('../../../mock/Response')
+
+module.exports = (handler, defaults = {}) => function test ({ request, mapping, configuration, match, redirect } = {}) {
+  if (typeof request === 'string') {
+    request = { method: 'GET', url: request }
+  }
+  request = new Request(request)
+  const response = new Response()
+  mapping = { ...defaults.mapping, ...mapping }
+  let mappingReady
+  if (handler.validate) {
+    mappingReady = handler.validate(mapping)
+  } else {
+    mappingReady = Promise.resolve()
+  }
+  return mappingReady
+    .then(() => {
+      const promise = handler.redirect({
+        configuration,
+        match,
+        request,
+        response,
+        mapping,
+        redirect: redirect || request.url
+      })
+      return { promise, response }
+    })
+}
+  
