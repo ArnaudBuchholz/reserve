8d0da8d4a005b809923677fd9c5bc22fd4c52cc4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-08 23:41:45 -0400

    Use handle

--- tests/mocha/handlers/file.test.js
+++ tests/mocha/handlers/file.test.js
@@ -5,44 +5,21 @@ const mime = require('../../../detect/mime')
 const Request = require('../../../mock/Request')
 const Response = require('../../../mock/Response')
 const fileHandler = require('../../../handlers/file')
+const handle = require('./handle')(fileHandler, { mapping: { cwd: '/' } })
 const fs = require('fs')
 
 const textMimeType = mime.getType('text')
 const htmlMimeType = mime.getType('html')
 const defaultMimeType = mime.getType('bin')
 
-function test (redirect, mapping = { cwd: '/' }, method = 'GET') {
-  const request = new Request(method)
-  const response = new Response()
-  return fileHandler.validate(mapping)
-    .then(() => {
-      const promise = fileHandler.redirect({
-        request,
-        response,
-        mapping,
-        redirect
-      })
-      return { promise, response }
-    })
-}
-
 describe('handlers/file', () => {
   it('returns a promise', async () => {
-    const { promise } = await test('./file.txt')
+    const { promise } = await handle('./file.txt')
     assert(() => typeof promise.then === 'function')
   })
 
-  it('pipes file content (relative path)', () => test('./file.txt')
-    .then(({ promise, response }) => promise.then(value => {
-      assert(() => value === undefined)
-      assert(() => response.statusCode === 200)
-      assert(() => response.headers['Content-Type'] === textMimeType)
-      assert(() => response.toString() === 'Hello World!')
-    }))
-  )
-
-  it('pipes file content (absolute path)', () => test('/file.txt', {
-    cwd: '/folder'
+  it('pipes file content (relative path)', () => handle({
+    request: './file.txt'
   })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === undefined)
@@ -52,7 +29,12 @@ describe('handlers/file', () => {
     }))
   )
 
-  it('pipes file content (trim parameters)', () => test('/file.txt?param=1#hash')
+  it('pipes file content (absolute path)', () => handle({
+    request: '/file.txt',
+    mapping: {
+      cwd: '/folder'
+    }
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === undefined)
       assert(() => response.statusCode === 200)
@@ -61,19 +43,36 @@ describe('handlers/file', () => {
     }))
   )
 
-  it('checks file access (url must *not* end with /)', () => test('./file.txt/')
+  it('pipes file content (trim parameters)', () => handle({
+    request: '/file.txt?param=1#hash'
+  })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 200)
+      assert(() => response.headers['Content-Type'] === textMimeType)
+      assert(() => response.toString() === 'Hello World!')
+    }))
+  )
+
+  it('checks file access (url must *not* end with /)', () => handle({
+    request: './file.txt/'
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === 404)
     }))
   )
 
-  it('checks folder access (url must end with /)', () => test('./folder')
+  it('checks folder access (url must end with /)', () => handle({
+    request: './folder'
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === 404)
     }))
   )
 
-  it('sends index.html if accessing a folder', () => test('./folder/')
+  it('sends index.html if accessing a folder', () => handle({
+    request: './folder/'
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === undefined)
       assert(() => response.statusCode === 200)
@@ -83,7 +82,12 @@ describe('handlers/file', () => {
     }))
   )
 
-  it('sends index.html if accessing a folder (HEAD)', () => test('./folder/', undefined, 'HEAD')
+  it('sends index.html if accessing a folder (HEAD)', () => handle({
+    request: {
+      method: 'HEAD',
+      url: './folder/'
+    }
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === undefined)
       assert(() => response.statusCode === 200)
@@ -93,7 +97,9 @@ describe('handlers/file', () => {
     }))
   )
 
-  it('defaults mimetype when no extension', () => test('./file')
+  it('defaults mimetype when no extension', () => handle({
+    request: './file'
+  })
     .then(({ promise, response }) => promise.then(value => {
       assert(() => value === undefined)
       assert(() => response.statusCode === 200)
@@ -102,25 +108,33 @@ describe('handlers/file', () => {
     }))
   )
 
-  it('fails with 404 if the file does not exist', () => test('./not-found')
+  it('fails with 404 if the file does not exist', () => handle({
+    request: './not-found'
+  })
     .then(({ promise }) => promise.then(value => {
       assert(() => value === 404)
     }))
   )
 
-  it('fails with 404 if the folder does not exist', () => test('./not-a-folder/not-found')
+  it('fails with 404 if the folder does not exist', () => handle({
+    request: './not-a-folder/not-found'
+  })
     .then(({ promise }) => promise.then(value => {
       assert(() => value === 404)
     }))
   )
 
-  it('fails with 404 if the folder does not have index.html', () => test('./no-index/')
+  it('fails with 404 if the folder does not have index.html', () => handle({
+    request: './no-index/'
+  })
     .then(({ promise }) => promise.then(value => {
       assert(() => value === 404)
     }))
   )
 
-  it('fails with 404 if the folder contains a sub folder named index.html', () => test('./wrong-index/')
+  it('fails with 404 if the folder contains a sub folder named index.html', () => handle({
+    request: './wrong-index/'
+  })
     .then(({ promise }) => promise.then(value => {
       assert(() => value === 404)
     }))
@@ -131,38 +145,8 @@ describe('handlers/file', () => {
       fs.setCaseSensitive(false)
     })
 
-    it('finds the file even if the file name does not match case sensitively', () => test('/File.txt')
-      .then(({ promise, response }) => promise.then(value => {
-        assert(() => value === undefined)
-        assert(() => response.statusCode === 200)
-        assert(() => response.headers['Content-Type'] === textMimeType)
-        assert(() => response.headers['Content-Length'] === 12)
-        assert(() => response.toString() === 'Hello World!')
-      }))
-    )
-
-    it('finds the file even if the file name does not match case sensitively (HEAD)', () => test('/File.txt', undefined, 'HEAD')
-      .then(({ promise, response }) => promise.then(value => {
-        assert(() => value === undefined)
-        assert(() => response.statusCode === 200)
-        assert(() => response.headers['Content-Type'] === textMimeType)
-        assert(() => response.headers['Content-Length'] === 12)
-        assert(() => response.toString() === '')
-      }))
-    )
-
-    it('fails with 404 if the file name does not match case sensitively but case-sensitive is set', () => test('/File.txt', {
-      cwd: '/',
-      'case-sensitive': true
-    })
-      .then(({ promise, response }) => promise.then(value => {
-        assert(() => value === 404)
-      }))
-    )
-
-    it('finds the file case sensitively when requested', () => test('/file.txt', {
-      cwd: '/',
-      'case-sensitive': true
+    it('finds the file even if the file name does not match case sensitively', () => handle({
+      request: '/File.txt'
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
@@ -173,10 +157,12 @@ describe('handlers/file', () => {
       }))
     )
 
-    it('finds the file case sensitively when requested (HEAD)', () => test('/file.txt', {
-      cwd: '/',
-      'case-sensitive': true
-    }, 'HEAD')
+    it('finds the file even if the file name does not match case sensitively (HEAD)', () => handle({
+      request: {
+        method: 'HEAD',
+        url: '/File.txt'
+      }
+    })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
         assert(() => response.statusCode === 200)
@@ -186,18 +172,71 @@ describe('handlers/file', () => {
       }))
     )
 
-    it('fails with 404 if the file name does not match case sensitively but case-sensitive is set (folder)', () => test('/Folder/index.html', {
-      cwd: '/',
-      'case-sensitive': true
+    it('fails with 404 if the file name does not match case sensitively but case-sensitive is set', () => handle({
+      request: '/File.txt',
+      mapping: {
+        cwd: '/',
+        'case-sensitive': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === 404)
       }))
     )
 
-    it('finds the file case sensitively when requested (folder)', () => test('/folder/index.html', {
-      cwd: '/',
-      'case-sensitive': true
+    it('finds the file case sensitively when requested', () => handle({
+      request: '/file.txt', 
+      mapping: {
+        cwd: '/',
+        'case-sensitive': true
+      }
+    })
+      .then(({ promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === textMimeType)
+        assert(() => response.headers['Content-Length'] === 12)
+        assert(() => response.toString() === 'Hello World!')
+      }))
+    )
+
+    it('finds the file case sensitively when requested (HEAD)', () => handle({
+      request: {
+        method: 'HEAD',
+        url: '/file.txt'
+      },
+      mapping: {
+        cwd: '/',
+        'case-sensitive': true
+      }
+    })
+      .then(({ promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers['Content-Type'] === textMimeType)
+        assert(() => response.headers['Content-Length'] === 12)
+        assert(() => response.toString() === '')
+      }))
+    )
+
+    it('fails with 404 if the file name does not match case sensitively but case-sensitive is set (folder)', () => handle({
+      request: '/Folder/index.html',
+      mapping: {
+        cwd: '/',
+        'case-sensitive': true
+      }
+    })
+      .then(({ promise, response }) => promise.then(value => {
+        assert(() => value === 404)
+      }))
+    )
+
+    it('finds the file case sensitively when requested (folder)', () => handle({
+      request: '/folder/index.html',
+      mapping: {
+        cwd: '/',
+        'case-sensitive': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
@@ -213,9 +252,12 @@ describe('handlers/file', () => {
   })
 
   describe('ignore-if-not-found', function () {
-    it('does not fail with 404 if the file does not exist', () => test('./not-found', {
-      cwd: '/',
-      'ignore-if-not-found': true
+    it('does not fail with 404 if the file does not exist', () => handle({
+      request: './not-found',
+      mapping: {
+        cwd: '/',
+        'ignore-if-not-found': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
@@ -223,9 +265,12 @@ describe('handlers/file', () => {
       }))
     )
 
-    it('does not fail with 404 if the folder does not exist', () => test('./not-a-folder/not-found', {
-      cwd: '/',
-      'ignore-if-not-found': true
+    it('does not fail with 404 if the folder does not exist', () => handle({
+      request: './not-a-folder/not-found',
+      mapping: {
+        cwd: '/',
+        'ignore-if-not-found': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
@@ -233,18 +278,24 @@ describe('handlers/file', () => {
       }))
     )
 
-    it('still fails for incorrect folder access (url must end with /)', () => test('./folder', {
-      cwd: '/',
-      'ignore-if-not-found': true
+    it('still fails for incorrect folder access (url must end with /)', () => handle({
+      request: './folder',
+      mapping: {
+        cwd: '/',
+        'ignore-if-not-found': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === 404)
       }))
     )
 
-    it('does not fail with 404 if the folder does not have index.html', () => test('./no-index/', {
-      cwd: '/',
-      'ignore-if-not-found': true
+    it('does not fail with 404 if the folder does not have index.html', () => handle({
+      request: './no-index/',
+      mapping: {
+        cwd: '/',
+        'ignore-if-not-found': true
+      }
     })
       .then(({ promise, response }) => promise.then(value => {
         assert(() => value === undefined)
