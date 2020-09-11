17b26d1b660b37c2337d0712b799e6860ad3f612
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-10 23:31:49 -0400

    Migrate to handler

--- tests/mocha/handlers/url.test.js
+++ tests/mocha/handlers/url.test.js
@@ -6,93 +6,90 @@ const assert = require('../assert')
 const Request = require('../../../mock/Request')
 const Response = require('../../../mock/Response')
 const urlHandler = require('../../../handlers/url')
-const initMapping = require('./initMapping.js')(urlHandler)
+const handle = require('./handle')(urlHandler)
 
 describe('handlers/url', () => {
-  it('returns a promise', async () => {
-    const request = new Request()
-    const response = new Response()
-    const mapping = await initMapping({})
-    const result = urlHandler.redirect({
-      request,
-      response,
-      mapping,
-      redirect: http.urls.empty
+  it('returns a promise', () => handle(http.urls.echo)
+    .then(({ promise }) => {
+      assert(() => typeof promise.then === 'function')
     })
-    assert(() => typeof result.then === 'function')
-  })
+  )
 
-  it('pipes URL content', async () => {
-    const request = new Request('GET', 'http://example.com/whatever', {
-      'x-status-code': 200,
-      'x-value-1': 'test',
-      host: 'http://example.com'
-    }, 'Hello World!')
-    const response = new Response()
-    const mapping = await initMapping({})
-    const value = await urlHandler.redirect({
-      request,
-      response,
-      mapping,
-      redirect: http.urls.echo
-    })
-    assert(() => value === undefined)
-    assert(() => response.statusCode === 200)
-    assert(() => response.headers['x-value-1'] === 'test')
-    assert(() => response.headers.host === undefined)
-    assert(() => response.toString() === 'Hello World!')
+  it('pipes URL content', () => handle({
+    request: {
+      method: 'POST',
+      url: http.urls.echo,
+      headers: {
+        'x-status-code': 200,
+        'x-value-1': 'test',
+        host: 'http://example.com'
+      },
+      body: 'Hello World!'
+    }
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 200)
+      assert(() => response.headers['x-value-1'] === 'test')
+      assert(() => response.headers.host === undefined)
+      assert(() => response.toString() === 'Hello World!')
+    }))
+  )
 
-  it('pipes URL content (https)', async () => {
-    const request = new Request('GET', 'http://example.com/whatever', {
-      'x-status-code': 200
-    }, 'Hello World!')
-    const response = new Response()
-    const mapping = await initMapping({})
-    const value = await urlHandler.redirect({
-      request,
-      response,
-      mapping,
-      redirect: http.urls.echos
-    })
-    assert(() => value === undefined)
-    assert(() => response.statusCode === 200)
-    assert(() => response.toString() === 'Hello World!')
+  it('pipes URL content (https)', () => handle({
+    request: {
+      method: 'POST',
+      url: http.urls.echo,
+      headers: {
+        'x-status-code': 200
+      },
+      body: 'Hello World!'
+    }
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 200)
+      assert(() => response.toString() === 'Hello World!')
+    }))
+  )
 
-  it('unsecures cookies', async () => {
-    const request = new Request('GET', 'http://example.com/abwhatever', {
-      'x-status-code': 200,
-      'Set-Cookie': ['name=value; Secure;']
-    })
-    const response = new Response()
-    const mapping = await initMapping({
-      'unsecure-cookies': true
-    })
-    const value = await urlHandler.redirect({
-      request,
-      response,
-      mapping,
-      redirect: http.urls.echos
-    })
-    assert(() => value === undefined)
-    assert(() => response.statusCode === 200)
-    assert(() => response.headers['Set-Cookie'][0] === 'name=value;')
+  it('unsecures cookies', () => handle({
+    request: {
+      method: 'GET',
+      url: http.urls.echos,
+      headers: {
+        'x-status-code': 200,
+        'Set-Cookie': ['name=value; Secure;']
+      }
+    },
+    mapping: {
+      'unsecure-cookies': true      
+    }
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 200)
+      assert(() => response.headers['Set-Cookie'][0] === 'name=value;')
+    }))
+  )
 
-  it('manipulates request details (forward-request)', async () => {
-    const request = new Request('GET', 'http://example.com/abwhatever', {
-      'x-status-code': 200,
-      Cookie: 'name=value;'
-    })
-    const response = new Response()
-    const configuration = {}
-    const match = {}
-    const mapping = await initMapping({
+  const uid = Symbol('uid')
+
+  it('manipulates request details (forward-request)', () => handle({
+    request: {
+      method: 'GET',
+      url: http.urls.echos,
+      headers: {
+        'x-status-code': 200,
+        Cookie: 'name=value;'
+      }
+    },
+    mapping: {
+      [uid]: 'mapping',
       'forward-request': async ({ configuration: receivedConfiguration, context, mapping: receivedMapping, match: receivedMatch, request }) => {
-        assert(() => receivedConfiguration === configuration)
-        assert(() => receivedMapping === mapping)
-        assert(() => receivedMatch === match)
+        assert(() => receivedConfiguration[uid] === 'configuration')
+        assert(() => receivedMapping[uid] === 'mapping')
+        assert(() => receivedMatch[uid] === 'match')
         assert(() => context && typeof context === 'object')
         assert(() => request.method === 'GET')
         assert(() => request.url === http.urls.echos)
@@ -100,67 +97,54 @@ describe('handlers/url', () => {
         assert(() => request.headers.Cookie === 'name=value;')
         request.headers.Cookie = 'a=b;c=d;'
       }
-    })
-    const value = await urlHandler.redirect({
-      configuration,
-      match,
-      request,
-      response,
-      mapping,
-      redirect: http.urls.echos
-    })
-    assert(() => value === undefined)
-    assert(() => response.statusCode === 200)
-    assert(() => response.headers.Cookie === 'a=b;c=d;')
+    },
+    configuration: { [uid]: 'configuration' },
+    match: { [uid]: 'match' }
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 200)
+      assert(() => response.headers.Cookie === 'a=b;c=d;')
+    }))
+  )
 
-  it('manipulates response headers (forward-response)', async () => {
-    const request = new Request('GET', 'http://example.com/abwhatever', {
-      'x-status-code': 200,
-      Cookie: 'name=value;'
-    })
-    const response = new Response()
-    const configuration = {}
-    const match = {}
+  it('manipulates response headers (forward-response)', () => {
     mockRequire('/url.forward-response.js', ({ configuration: receivedConfiguration, context, mapping: receivedMapping, match: receivedMatch, headers }) => {
-      assert(() => receivedConfiguration === configuration)
-      assert(() => receivedMapping === mapping)
-      assert(() => receivedMatch === match)
+      assert(() => receivedConfiguration[uid] === 'configuration')
+      assert(() => receivedMapping[uid] === 'mapping')
+      assert(() => receivedMatch[uid] === 'match')
       assert(() => context && typeof context === 'object')
       assert(() => headers['x-status-code'] === 200)
       assert(() => headers.Cookie === 'name=value;')
       headers.Cookie = 'a=b;c=d;'
     })
-    const mapping = await initMapping({
-      'forward-response': '/url.forward-response.js'
+    return handle({
+      request: {
+        method: 'GET',
+        url: http.urls.echos,
+        headers: {
+          'x-status-code': 200,
+          Cookie: 'name=value;'
+        }
+      },
+      mapping: {
+        [uid]: 'mapping',
+        'forward-response': '/url.forward-response.js'
+      },
+      configuration: { [uid]: 'configuration' },
+      match: { [uid]: 'match' }
     })
-    const value = await urlHandler.redirect({
-      configuration,
-      match,
-      request,
-      response,
-      mapping,
-      redirect: http.urls.echos
-    })
-    assert(() => value === undefined)
-    assert(() => response.statusCode === 200)
-    assert(() => response.headers.Cookie === 'a=b;c=d;')
+      .then(({ promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.statusCode === 200)
+        assert(() => response.headers.Cookie === 'a=b;c=d;')
+      }))
   })
 
-  it('provides a unique context to link forward-request and forward-response', async () => {
-    const request1 = new Request('GET', 'http://example.com/abwhatever', {
-      'x-status-code': 200,
-      'x-id': 1
-    })
-    const response1 = new Response()
-    const request2 = new Request('GET', 'http://example.com/abwhatever', {
-      'x-status-code': 200,
-      'x-id': 2
-    })
-    const response2 = new Response()
+  it('provides a unique context to link forward-request and forward-response', () => {
     let contextId = 0
     const checks = {}
-    const mapping = await initMapping({
+    const mapping = {
       'forward-request': ({ context, request }) => {
         assert(() => context.id === undefined)
         context.id = ++contextId
@@ -174,20 +158,30 @@ describe('handlers/url', () => {
         assert(() => context.requestId !== undefined)
         assert(() => checks[context.id] === context.requestId)
       }
-    })
+    }
     return Promise.all([
-      urlHandler.redirect({
-        request: request1,
-        response: response1,
-        mapping,
-        redirect: http.urls.echos
-      }),
-      urlHandler.redirect({
-        request: request2,
-        response: response2,
-        mapping,
-        redirect: http.urls.echos
-      })
+      handle({
+        request: {
+          method: 'GET',
+          url: http.urls.echos,
+          headers: {
+            'x-status-code': 200,
+            'x-id': 1
+          }
+        },
+        mapping
+      }).then(({ promise }) => promise),
+      handle({
+        request: {
+          method: 'GET',
+          url: http.urls.echos,
+          headers: {
+            'x-status-code': 200,
+            'x-id': 2
+          }
+        },
+        mapping
+      }).then(({ promise }) => promise)
     ])
   })
 })
