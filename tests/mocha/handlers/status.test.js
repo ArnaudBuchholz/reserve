2d8c12c7a11202f5bdd2541b9b96a5560ab86917
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-10 23:31:40 -0400

    Migrate to handler

--- tests/mocha/handlers/status.test.js
+++ tests/mocha/handlers/status.test.js
@@ -2,58 +2,61 @@
 
 const assert = require('../assert')
 const mime = require('../../../detect/mime')
-const Response = require('../../../mock/Response')
 const statusHandler = require('../../../handlers/status')
+const handle = require('./handle')(statusHandler)
 
 const textMimeType = mime.getType('text')
 
 describe('handlers/status', () => {
-  it('returns a promise', () => {
-    const response = new Response()
-    const result = statusHandler.redirect({ response, redirect: 404 })
-    assert(() => typeof result.then === 'function')
+  it('returns a promise', () => handle({
+    mapping: null,
+    redirect: 404
   })
+    .then(({ promise }) => {
+      assert(() => typeof promise.then === 'function')
+    })
+  )
 
-  it('sends a generic response for knonw codes', () => {
-    const response = new Response()
-    return statusHandler.redirect({ response, redirect: 404 })
-      .then(async value => {
-        assert(() => value === undefined)
-        await response.waitForFinish()
-        assert(() => response.statusCode === 404)
-        assert(() => response.headers['Content-Type'] === textMimeType)
-        assert(() => response.toString() === 'Not found')
-      })
+  it('sends a generic response for knonw codes', () => handle({
+    mapping: null,
+    redirect: 404
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 404)
+      assert(() => response.headers['Content-Type'] === textMimeType)
+      assert(() => response.toString() === 'Not found')
+    }))
+  )
 
-  it('sends an empty response for unknonw codes', () => {
-    const response = new Response()
-    return statusHandler.redirect({ response, redirect: 418 })
-      .then(async value => {
-        assert(() => value === undefined)
-        await response.waitForFinish()
-        assert(() => response.statusCode === 418)
-        assert(() => response.headers['Content-Type'] === textMimeType)
-        assert(() => response.toString() === '')
-      })
+  it('sends an empty response for unknonw codes',  () => handle({
+    mapping: null,
+    redirect: 418
   })
-
-  it('can be used for redirections', () => {
-    const response = new Response()
-    const mapping = {
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 418)
+      assert(() => response.headers['Content-Type'] === textMimeType)
+      assert(() => response.toString() === '')
+    }))
+  )
+  
+  it('can be used for redirections',  () => handle({
+    mapping: {
+      status: 302,
       headers: {
         Location: '$1'
       }
-    }
-    const match = [undefined, 'https://www.npmjs.com/package/reserve']
-    return statusHandler.redirect({ mapping, match, response, redirect: 302 })
-      .then(async value => {
-        assert(() => value === undefined)
-        await response.waitForFinish()
-        assert(() => response.statusCode === 302)
-        assert(() => response.headers['Content-Type'] === textMimeType)
-        assert(() => response.toString() === '')
-        assert(() => response.headers.Location === 'https://www.npmjs.com/package/reserve')
-      })
+    },
+    redirect: 302,
+    match: [undefined, 'https://www.npmjs.com/package/reserve']    
   })
+    .then(({ promise, response }) => promise.then(value => {
+      assert(() => value === undefined)
+      assert(() => response.statusCode === 302)
+      assert(() => response.headers['Content-Type'] === textMimeType)
+      assert(() => response.toString() === '')
+      assert(() => response.headers.Location === 'https://www.npmjs.com/package/reserve')
+    }))
+  )
 })
