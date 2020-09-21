31c061e6f4e61bc711631217c82f0ce42222f7ff
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-21 00:43:52 -0400

    Build proper match method (#13)

--- mapping.js
+++ mapping.js
@@ -2,11 +2,13 @@
 
 const { validate } = require('./schema')
 const checkMethod = require('./checkMethod')
+const buildMatch = require('./buildMatch')
 const {
   $configurationInterface,
   $handlerMethod,
   $handlerSchema,
   $mappingChecked,
+  $mappingMatch,
   $mappingMethod
 } = require('./symbols')
 
@@ -29,7 +31,7 @@ const matchTypes = {
   },
   object: (mapping, match) => {
     if (!(mapping.match instanceof RegExp)) {
-      throw new Error('')
+      invalidMatch()
     }
   }
 }
@@ -44,6 +46,13 @@ function checkMatch (mapping) {
   matchTypes[typeof mapping.match](mapping, mapping.match)
 }
 
+function checkInvertMatch (mapping) {
+  const invertMatch = mapping['invert-match']
+  if (invertMatch !== undefined && invertMatch !== true) {
+    throw new Error('Invalid mapping invert-match')
+  }
+}
+
 function checkHandler (configuration, mapping) {
   const { handler } = configuration.handler(mapping)
   if (!handler) {
@@ -56,6 +65,7 @@ module.exports = {
   async check (configuration, mapping) {
     checkCwd(mapping)
     checkMatch(mapping)
+    checkInvertMatch(mapping)
     const handler = checkHandler(configuration, mapping)
     checkMethod(mapping, $mappingMethod, handler[$handlerMethod])
     if (handler[$handlerSchema]) {
@@ -64,6 +74,7 @@ module.exports = {
     if (handler.validate) {
       await handler.validate(mapping, configuration[$configurationInterface])
     }
+    mapping[$mappingMatch] = buildMatch(mapping)
     mapping[$mappingChecked] = true
   }
 }
