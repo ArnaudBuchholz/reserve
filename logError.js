4bf5a64fe480ed668d1956b2d0142f50769529ed
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-26 00:28:25 -0400

    Enables verbose to output request ID

--- logError.js
+++ logError.js
@@ -3,10 +3,10 @@
 const { gray, red } = require('./detect/colors')
 const logCommon = require('./logCommon')
 
-module.exports = event => {
+module.exports = (event, verbose = false) => {
   const reason = event.reason
   if (event.method && event.url) {
-    logCommon.call(event, 'ERROR', false, red('\n\\____'), gray(reason.toString()))
+    logCommon.call(event, 'ERROR', verbose, red('\n\\____'), gray(reason.toString()))
   } else {
     logCommon.call({ ...event, method: '', url: '' }, 'ERROR', false, gray(reason.toString()))
   }
