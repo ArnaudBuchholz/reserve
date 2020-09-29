7afd2706a89aa732f77baf8e9522c80ab5d45b8c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-29 06:10:56 -0400

    Rename (#23)

--- tests/mocha/lastModified.test.js
+++ tests/mocha/lastModified.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const assert = require('./assert')
-const { format, parse } = require('../../mtime')
+const { format, parse } = require('../../lastModified')
 
 const dates = [{
   d: new Date(Date.UTC(2015, 9, 21, 7, 28, 0, 0)),
@@ -9,9 +9,15 @@ const dates = [{
 }, {
   d: new Date(Date.UTC(2003, 0, 21, 23, 35, 8, 0)),
   t: 'Tue, 21 Jan 2003 23:35:08 GMT'
+}, {
+  d: new Date(Date.UTC(2020, 0, 1, 0, 0, 0, 0)),
+  t: 'Wed, 01 Jan 2020 00:00:00 GMT'
+}, {
+  d: new Date(Date.UTC(2019, 11, 31, 23, 59, 59, 0)),
+  t: 'Tue, 31 Dec 2019 23:59:59 GMT'
 }]
 
-describe('mtime', () => {
+describe('lastModified', () => {
   describe('format', () => {
     dates.forEach(date => it(`formats '${date.t}'`, () => {
       assert(() => format(date.d) === date.t)
