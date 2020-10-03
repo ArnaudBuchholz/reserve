cbbe8edac519c2953d28267c3aa37aac507f9f61
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-02 22:32:31 -0400

    Live test with caching (FIX #23)

---
+++ tests/cache.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { mkdir, writeFile } = require('fs')
+const { join } = require('path')
+const { log, serve } = require('..')
+const { promisify } = require('util')
+
+const mkdirAsync = promisify(mkdir)
+const writeFileAsync = promisify(writeFile)
+
+const cacheBasePath = join(__dirname, 'cache')
+const cacheFilePath = join(cacheBasePath, 'cache.txt')
+
+async function main () {
+  await mkdirAsync(cacheBasePath, { recursive: true })
+  await writeFileAsync(cacheFilePath, `${new Date().toISOString()}
+Hello World !`)
+
+  log(serve({
+    port: 8006,
+    mappings: [{
+      match: /^\//,
+      file: cacheFilePath,
+      'caching-strategy': 'modified'
+    }]
+  }), process.argv.includes('--verbose'))
+  
+}
+
+main()
