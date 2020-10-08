5af2eb075eb6283073cad3c0279a1ddc1890e81e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-08 07:27:05 -0400

    Verbose error on one line only

--- logError.js
+++ logError.js
@@ -6,7 +6,13 @@ const logCommon = require('./logCommon')
 module.exports = (event, verbose = false) => {
   const reason = event.reason
   if (event.method && event.url) {
-    logCommon.call(event, 'ERROR', verbose, red('\n\\____'), gray(reason.toString()))
+    const details = []
+    if (verbose) {
+      details.push(red(reason.toString()))
+    } else {
+      details.push(red('\n\\____'), gray(reason.toString()))
+    }
+    logCommon.call(event, 'ERROR', verbose, ...details)
   } else {
     logCommon.call({ ...event, method: '', url: '' }, 'ERROR', false, gray(reason.toString()))
   }
