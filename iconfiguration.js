4e9634708ea4c5795588544fbeb4a1d0be5a0d3f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-31 01:04:26 -0500

    Implements detection and ensure requests are not blocked (#39)

--- iconfiguration.js
+++ iconfiguration.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { gray, red, green } = require('./detect/colors')
 const { check } = require('./mapping')
 const {
   $configuration,
@@ -28,17 +29,35 @@ module.exports = class IConfiguration {
     return [].concat(this[$configuration].mappings)
   }
 
-  async setMappings (mappings, request) {
+  async setMappings (mappings, request, timeout = 5000) {
     const configuration = this[$configuration]
     await checkMappings(configuration, mappings)
     const configurationRequests = configuration[$configurationRequests]
     const { contexts } = configurationRequests
     const requestContext = contexts.filter(({ request: candidate }) => candidate === request)[0]
     const requestsHolding = contexts.filter(candidate => candidate !== requestContext).map(({ holding }) => holding)
-    configurationRequests.holding = Promise.all(requestsHolding)
+    const holding = Promise.race([
+      Promise.all(requestsHolding),
+      new Promise((resolve, reject) => setTimeout(() => {
+        reject(new Error('iconfiguration.setMappings appears to be blocked'))
+      }, timeout))
+    ])
       .then(() => {
         configuration.mappings = mappings
       })
-    return configurationRequests.holding
+    configurationRequests.holding = holding.then(undefined, () => {}) // Absorb error to unblock server
+    holding.then(undefined, () => {
+      console.log(red('REserve detected a long holding request during configuration.setMappings'))
+      contexts.forEach(({ request, released }) => {
+        let status
+        if (released) {
+          status = green('excluded from holding list')
+        } else {
+          status = ''
+        }
+        console.log(gray(request.method), gray(request.url), status)
+      })
+    })
+    return holding
   }
 }
