d6bf77c261f805124eb0dfbfdb40285759b0e17f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-15 00:39:10 -0500

    Linter fix

--- tests/mocha/configuration.test.js
+++ tests/mocha/configuration.test.js
@@ -97,7 +97,7 @@ describe('configuration', () => {
 
     it('supports http2', () => {
       return read('/folder/reserve.json')
-        .then(configuration => check({ ...configuration, http2: true }))
+        .then(configuration => check({ ...configuration, http2: true }))
         .then(configuration => {
           assert(() => configuration.ssl.key === 'privatekey')
           assert(() => configuration.ssl.cert === 'certificate')
