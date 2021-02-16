efcf83b5ef9eeb64cc3b0001d564494f002adf39
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-15 21:09:08 -0500

    Exposes HTTP2 setting

--- iconfiguration.js
+++ iconfiguration.js
@@ -29,6 +29,10 @@ module.exports = class IConfiguration {
     return [].concat(this[$configuration].mappings)
   }
 
+  get http2 () {
+    return this[$configuration].http2 || false
+  }
+
   async setMappings (mappings, request, timeout = 5000) {
     const configuration = this[$configuration]
     await checkMappings(configuration, mappings)
