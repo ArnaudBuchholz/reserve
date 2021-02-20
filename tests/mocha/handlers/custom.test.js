9b8751bfbe243e37641fc44ed581b1bb4d2eb66a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-20 00:46:05 -0500

    Adapt to handle, test configuration interface

--- tests/mocha/handlers/custom.test.js
+++ tests/mocha/handlers/custom.test.js
@@ -2,16 +2,17 @@
 
 const mockRequire = require('mock-require')
 const assert = require('../assert')
-const Request = require('../../../mock/Request')
-const Response = require('../../../mock/Response')
 const customHandler = require('../../../handlers/custom')
+const handle = require('./handle')(customHandler, {
+  mapping: {
+    custom: () => {}
+  }
+})
 
-const { $customCallback, $customTimestamp } = require('../../../symbols')
-
-/* istanbul ignore next */ // We don't expect it to happen !
-function unexpectedCall () {
-  assert(() => false)
-}
+const {
+  $configuration,
+  $customTimestamp
+} = require('../../../symbols')
 
 describe('handlers/custom', () => {
   it('validates the custom function', async () => {
@@ -26,153 +27,151 @@ describe('handlers/custom', () => {
     assert(() => !!exceptionCaught)
   })
 
-  it('returns a promise', () => {
-    const request = new Request()
-    const response = new Response()
-    const result = customHandler.redirect({ request, response, mapping: { [$customCallback]: () => {} }, match: [] })
-    assert(() => typeof result.then === 'function')
-  })
-
-  it('calls the function with the proper parameters', () => {
-    const request = new Request()
-    const response = new Response()
-    return customHandler.redirect({
-      request,
-      response,
-      mapping: {
-        [$customCallback]: (receivedRequest, receivedResponse, ...additionalParameters) => {
-          assert(() => receivedRequest === request)
-          assert(() => receivedResponse === response)
-          assert(() => additionalParameters.length === 2)
-          assert(() => additionalParameters[0] === 'first')
-          assert(() => additionalParameters[1] === 'second')
-        }
-      },
-      match: ['capturing groups are: ', 'first', 'second']
+  it('returns a promise', () => handle({ request: '/any' })
+    .then(({ promise }) => {
+      assert(() => typeof promise.then === 'function')
     })
-  })
+  )
 
-  it('waits for the function result if a promise', () => {
-    const request = new Request()
-    const response = new Response()
-    return customHandler.redirect({
-      request,
-      response,
-      mapping: {
-        [$customCallback]: async () => 'OK'
-      },
-      match: []
-    })
-      .then(result => assert(() => result === 'OK'))
+  it('calls the function with the proper parameters', () => handle({
+    request: '/any',
+    match: ['capturing groups are: ', 'first', 'second'],
+    mapping: {
+      custom: function () {
+        return arguments
+      }
+    }
   })
+    .then(({ promise, request, response }) => promise.then(args => {
+      assert(() => args.length === 4)
+      assert(() => args[0] === request)
+      assert(() => args[1] === response)
+      assert(() => args[2] === 'first')
+      assert(() => args[3] === 'second')
+    }))
+  )
 
-  it('lets any exception flow (will be intercepted by dispatcher)', () => {
-    const request = new Request()
-    const response = new Response()
-    return customHandler.redirect({
-      request,
-      response,
-      mapping: {
-        [$customCallback]: () => {
-          throw new Error('KO')
-        }
-      },
-      match: []
-    })
-      .then(unexpectedCall)
-      .catch(reason => {
-        assert(() => reason.message === 'KO')
-      })
+  it('waits for the function result if a promise', () => handle({
+    request: '/any',
+    mapping: {
+      custom: async () => 'OK'
+    }
   })
+    .then(({ promise }) => promise.then(value => {
+      assert(() => value === 'OK')
+    }))
+  )
 
-  it('returns any rejected promise (will be intercepted by dispatcher)', () => {
-    const request = new Request()
-    const response = new Response()
-    return customHandler.redirect({
-      request,
-      response,
-      mapping: {
-        [$customCallback]: async () => {
-          throw new Error('KO')
-        }
-      },
-      match: []
-    })
-      .then(unexpectedCall)
-      .catch(reason => {
-        assert(() => reason.message === 'KO')
-      })
+  it('lets any exception flow (will be intercepted by dispatcher)', () => handle({
+    request: '/any',
+    mapping: {
+      custom: () => { throw new Error('KO') }
+    }
   })
+    .then(({ promise }) => promise.then(assert.notExpected, reason => {
+      assert(() => reason.message === 'KO')
+    }))
+  )
+
+  it('returns any rejected promise (will be intercepted by dispatcher)', () => handle({
+    request: '/any',
+    mapping: {
+      custom: async () => { throw new Error('KO') }
+    }
+  })
+    .then(({ promise }) => promise.then(assert.notExpected, reason => {
+      assert(() => reason.message === 'KO')
+    }))
+  )
 
   it('implements file watching (timestamp changes)', async () => {
-    const request = new Request()
-    const response1 = new Response()
     const mapping = {
       cwd: '/',
       custom: 'now.js',
       watch: true
     }
-    mockRequire('/now.js', (request, response) => response.end('first'))
-    await customHandler.validate(mapping)
-    await customHandler.redirect({
-      request,
-      response: response1,
-      mapping,
-      match: []
-    })
-    const timestamp1 = mapping[$customTimestamp]
-    await response1.waitForFinish()
-    assert(() => response1.toString() === 'first')
-    assert(() => timestamp1)
 
-    const response2 = new Response()
-    mockRequire('/now.js', (request, response) => response.end('second'))
-    await customHandler.redirect({
-      request,
-      response: response2,
-      mapping,
-      match: []
+    mockRequire('/now.js', (request, response) => { response.end('first') })
+    let timestamp1
+    await handle({
+      request: '/any',
+      mapping
     })
-    await response2.waitForFinish()
-    assert(() => response2.toString() === 'second')
-    assert(() => mapping[$customTimestamp] !== timestamp1)
+      .then(({ mapping, promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.toString() === 'first')
+        timestamp1 = mapping[$customTimestamp]
+        assert(() => timestamp1)
+      }))
+
+    mockRequire('/now.js', (request, response) => { response.end('second') })
+    await handle({
+      request: '/any',
+      mapping
+    })
+      .then(({ mapping, promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.toString() === 'second')
+        assert(() => mapping[$customTimestamp] !== timestamp1)
+      }))
   })
 
   it('implements file watching (timestamp remains the same)', async () => {
-    const request = new Request()
-    const response1 = new Response()
     const mapping = {
       cwd: '/',
       custom: 'not-now.js',
       watch: true
     }
-    mockRequire('/not-now.js', (request, response) => response.end('first'))
-    await customHandler.validate(mapping)
-    await customHandler.redirect({
-      request,
-      response: response1,
-      mapping,
-      match: []
-    })
-    const timestamp1 = mapping[$customTimestamp]
-    await response1.waitForFinish()
-    assert(() => response1.toString() === 'first')
-    assert(() => timestamp1)
 
-    const response2 = new Response()
-    /* istanbul ignore next */ // We don't expect it to be called !
-    function wontBeCalled (request, response) {
-      response.end('second')
-    }
-    mockRequire('/not-now.js', wontBeCalled)
-    await customHandler.redirect({
-      request,
-      response: response2,
-      mapping,
-      match: []
+    mockRequire('/not-now.js', (request, response) => { response.end('first') })
+    let timestamp1
+    await handle({
+      request: '/any',
+      mapping
     })
-    await response2.waitForFinish()
-    assert(() => response2.toString() === 'first')
-    assert(() => mapping[$customTimestamp] === timestamp1)
+      .then(({ mapping, promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.toString() === 'first')
+        timestamp1 = mapping[$customTimestamp]
+        assert(() => timestamp1)
+
+        mockRequire('/not-now.js', assert.notExpected)
+        return handle({
+          request: '/any',
+          mapping
+        })
+      }))
+      .then(({ mapping, promise, response }) => promise.then(value => {
+        assert(() => value === undefined)
+        assert(() => response.toString() === 'first')
+        assert(() => mapping[$customTimestamp] === timestamp1)
+      }))
   })
+
+  it('passes configuration on mapping if member not defined', () => handle({
+    request: '/any',
+    mapping: {
+      custom: () => {}
+    }
+  })
+    .then(({ mapping, promise }) => promise.then(() => {
+      assert(() => mapping.configuration[$configuration])
+      assert(() => !mapping.configuration[$customTimestamp])
+    }))
+  )
+
+  it('does not pass configuration on mapping if member is defined', () => handle({
+    request: '/any',
+    mapping: {
+      custom: () => {},
+      configuration: {
+        [$customTimestamp]: 'whatever'
+      }
+    }
+  })
+    .then(({ mapping, promise }) => promise.then(() => {
+      assert(() => mapping.configuration[$customTimestamp])
+      assert(() => !mapping.configuration[$configuration])
+    }))
+  )
 })
