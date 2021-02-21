50143b2e535ff9760156aaca1b9f6c77ed25006a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-21 09:20:57 -0500

    Use configuration.dispatch

--- samples/http2/push.js
+++ samples/http2/push.js
@@ -1,18 +1,14 @@
-module.exports = (request, response) => {
-  response.createPushResponse({
-    ':method': 'GET',
-    ':path': '/pushed.js'
-  }, (err, res) => {
-    setTimeout(function () {
-      console.log(err)
-      try {
-        res.writeHead(200, {
-          'content-type': 'application/javascript'
-        })
-        res.end('var msg = "Hello World !";')
-      } catch (e) {
-        console.error(e)
+const { Request } = require('../..')
+
+module.exports = function (request, response) {
+  for (let i = 0; i < 100; ++i) {
+    response.createPushResponse({
+      ':method': 'GET',
+      ':path': `/http2-${i}.svg`
+    }, (err, res) => {
+      if (!err) {
+        this.configuration.dispatch(new Request('GET', `/http2-${i}.svg`), res)
       }
-    }, 100)
-  })
+    })
+  }
 }
