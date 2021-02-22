ef52a2f26f78011e60a630409c0b58c89e3cf88a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-21 23:22:10 -0500

    Isolates push helper

--- samples/http2/push.js
+++ samples/http2/push.js
@@ -1,14 +1,18 @@
 const { Request } = require('../..')
 
+function push (configuration, path, response) {
+  response.createPushResponse({
+    ':method': 'GET',
+    ':path': path
+  }, (err, res) => {
+    if (!err) {
+      configuration.dispatch(new Request('GET', path), res)
+    }
+  })
+}
+
 module.exports = function (request, response) {
   for (let i = 0; i < 100; ++i) {
-    response.createPushResponse({
-      ':method': 'GET',
-      ':path': `/http2-${i}.svg`
-    }, (err, res) => {
-      if (!err) {
-        this.configuration.dispatch(new Request('GET', `/http2-${i}.svg`), res)
-      }
-    })
+    push(this.configuration, `/http2-${i}.svg`, response)
   }
 }
