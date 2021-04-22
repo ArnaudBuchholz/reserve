b34b5a6a4114a194fb5d13930434a2e27a45ab6e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-22 07:04:52 -0400

    Creates a plots storage

--- samples/canvas/plots.js
+++ samples/canvas/plots.js
@@ -1,62 +1,24 @@
-'use strict'
+const EventEmitter = require('events')
 
-const { body } = require('../..')
-
-const send = function (channel, id, plot) {
-  try {
-    channel.write(`event: plot\nid: ${id}\ndata: ${plot}\n\n`)
-  } catch (e) {
-    // ignore
-  }
-}
-
-module.exports = async function (request, response) {
-  if (!this.plots) {
-    this.channels = []
-    this.plots = []
+class Plots extends EventEmitter {
+  constructor () {
+    super()
+    this._plots = []
   }
 
-  if (request.method === 'POST') {
-    const plot = await body(request)
-    this.plots.push(plot)
-    const id = this.plots.length
-    this.channels.forEach(channel => send(channel, id, plot))
-    response.writeHead(200, {
-      'Content-Type': 'text/plain',
-      'Cache-Control': 'no-cache',
-      'Content-Length': 0
-    })
-    response.end()
-    return
+  push (plot) {
+    const id = this._plots.length
+    this._plots.push(plot)
+    this.emit('plot', { id, plot })
   }
 
-  const headers = {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache'
-  }
-  if (!this.configuration.http2) {
-    headers.Connection = 'keep-alive'
-  }
-  response.writeHead(200, headers)
-
-  const lastId = parseInt(request.headers['Last-Event-ID'] || '-1')
-  this.plots.slice(lastId + 1).forEach((plot, id) => send(response, id, plot))
-
-  this.channels.push(response)
-
-  let resolver
-  const promise = new Promise(resolve => {
-    resolver = resolve
-  })
-  const close = () => {
-    this.channels = this.channels.filter(channel => channel !== response)
-    try {
-      response.end()
-    } finally {
-      resolver()
+  forEach (from, handler) {
+    if (typeof from === 'function') {
+      handler = from
+      from = 0
     }
+    this._plots.slice(from).forEach((plot, id) => handler({ id, plot }))
   }
-  request.on('close', close)
-  request.on('abort', close)
-  return promise
 }
+
+module.exports = new Plots()
