56522692f469bb366d8c0ad03fb33b30a2be379a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-05-06 00:12:43 -0400

    documentation(samples): use config

--- samples/authent/login.js
+++ samples/authent/login.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { jwtExpiration, jwtSecret } = require('./config')
 const { body } = require('reserve')
 const jose = require('jose')
 const { setCookie, names: { jwt: $jwt } } = require('./cookies')
@@ -7,7 +8,6 @@ const { toLogin, toStartup } = require('./redirect')
 const { check } = require('./password')
 
 module.exports = async function login (request, response) {
-  const jwtExpiration = parseInt(process.env.JWT_EXPIRATION || '3600', 10)
   try {
     const payload = new URLSearchParams(await body(request))
     const user = payload.get('u')
@@ -20,7 +20,7 @@ module.exports = async function login (request, response) {
     })
       .setProtectedHeader({ alg: 'HS256' })
       .setExpirationTime(Date.now() + jwtExpiration * 1000)
-      .sign(Buffer.from(process.env.JWT_SECRET))
+      .sign(jwtSecret)
     setCookie(response, {
       name: $jwt,
       value: jwt
