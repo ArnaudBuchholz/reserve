aea3db49ce4823c77e27b58b9f9581ac33f83450
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-05-06 00:16:37 -0400

    documentation(samples): use config

--- samples/authent/authent.js
+++ samples/authent/authent.js
@@ -1,18 +1,16 @@
-require('dotenv')
+'use strict'
+
+const { jwtSecret } = require('./config')
 const jose = require('jose')
 const { getAllCookies, names: { jwt: $jwt } } = require('./cookies')
-const { toLogin } = require('redirect')
+const { toLogin } = require('./redirect')
 
-module.exports = {
-  async isAuthenticated (request, response) {
-    try {
-      const token = getAllCookies(request)[$jwt]
-      const { payload } = await jose.jwtVerify(token, Buffer.from(process.env.JWT_SECRET))
-      request.jwt = payload
-      return true
-    } catch (e) {
-      toLogin(request, response)
-    }
-    return false
+module.exports = async function isAuthenticated (request, response) {
+  try {
+    const token = getAllCookies(request)[$jwt]
+    const { payload } = await jose.jwtVerify(token, jwtSecret)
+    request.jwt = payload
+  } catch (e) {
+    toLogin(request, response)
   }
 }
