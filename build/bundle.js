a136300f4aa339c64c6f08df0f157a965e8ad2c9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-10-15 23:37:25 -0400

    build(bundle): generate a json description

--- build/bundle.js
+++ build/bundle.js
@@ -1,27 +1,49 @@
-const { readFileSync } = require('fs')
+'use strict'
+
+const { readFileSync, writeFileSync, mkdirSync } = require('fs')
 const { join, dirname } = require('path')
 
-const src = join(__dirname, '../src')
-const stack = [join(src, 'index.js')]
-const processed = {}
+const stack = ['index.js']
+const modules = {}
 
 while (stack.length) {
   const path = stack.shift()
-  if (Object.keys(processed).includes(path)) {
+  if (Object.keys(modules).includes(path)) {
     continue
   }
-  console.log(`[${path}]`)
-  const content = readFileSync(path).toString()
-  processed[path] = content
-  content.replace(/require\(\s*(.*)\s*\)/g, (_, dependency) => {
+  const content = readFileSync(join('src', path)).toString()
+  const module = {
+    // content
+  }
+  const dynamic = []
+  const native = {}
+  content.replace(/require\(\s*(.*)\s*\)/g, (instruction, dependency) => {
     if (dependency.startsWith('\'')) {
       const id = dependency.match(/^'(.*)'$/)[1]
-      console.log(id)
-      if (!id.startsWith('.')) {
-        console.log('module:', id)
+      if (id.startsWith('.')) {
+        stack.push(join(dirname(path), `${id}.js`).replace(/\\/g, '/'))
       } else {
-        stack.push(join(dirname(path), `${id}.js`))
+        const re = `(\\w+|\\{[^}]+\\})\\s*=\\s*require\\('${id.replace(/\//g, '\\/')}'\\)`
+        try {
+          const match = content.match(new RegExp(re), 'i')[1]
+          native[id] = match
+        } catch (e) {
+          console.error(e.toString(), path, re, id)
+          throw e
+        }
       }
+    } else {
+      dynamic.push(instruction)
     }
   })
+  if (Object.keys(native).length) {
+    module.native = native
+  }
+  if (dynamic.length) {
+    module.dynamic = dynamic
+  }
+  modules[path] = module
 }
+
+mkdirSync('dist', { recursive: true })
+writeFileSync('dist/bundle.json', JSON.stringify(modules, undefined, 2))
