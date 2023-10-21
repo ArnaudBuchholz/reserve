04da616bdd98ddf3a5eb973fec89b40b9ee46811
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-10-21 14:09:25 -0400

    feat(bundle): wip

--- build/bundle.js
+++ build/bundle.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const { readFileSync, writeFileSync, mkdirSync } = require('fs')
-const { join, dirname } = require('path')
+const { mkdirSync, readFileSync, writeFileSync } = require('fs')
+const { dirname, join } = require('path')
 
 const stack = ['index.js']
 const modules = {}
@@ -45,10 +45,34 @@ while (stack.length) {
   if (dynamic.length) {
     module.dynamic = dynamic
   }
-  delete module.content // TODO remove
-  delete module.depends // TODO remove
+  module.path = path
+  module.exports = `$export_${path.replace('.js', '').replace('/', '_')}`
   modules[path] = module
 }
 
 mkdirSync('dist', { recursive: true })
 writeFileSync('dist/bundle.json', JSON.stringify(modules, undefined, 2))
+writeFileSync('dist/core.js', '')
+
+const written = ['index.js', 'dependencies.js']
+const remaining = Object.values(modules).filter(({ path }) => !written.includes(path))
+
+while (remaining.length) {
+  const pos = remaining.findIndex(module => module.depends.every(dep => written.includes(dep)))
+  if (pos === -1) {
+    console.error('Unable to complete.')
+    process.exit(-1)
+  }
+  const { path, content, exports } = remaining[pos]
+  remaining.splice(pos, 1)
+  written.push(path)
+  writeFileSync('dist/core.js', `// BEGIN OF ${path}\n`, { flag: 'a' })
+  const transformed = content
+    .replace(/'use strict'\s*\n/g, '') // No more required
+    .replace(/const [^\n]*= require\('[^']+dependencies'\)/g, dependencies => `// ${dependencies}`) // No more required
+    .replace('module.exports', `const ${exports}`) // Convert to variable
+  writeFileSync('dist/core.js', transformed, { flag: 'a' })
+  writeFileSync('dist/core.js', `\n// END OF ${path}\n`, { flag: 'a' })
+}
+
+console.log('Done.')
