47772a62932383c12a79dd2d92f8481dcbfe75a6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-10-23 16:41:23 -0400

    fix(build): generate indexes

--- build/generate.js
+++ build/generate.js
@@ -3,7 +3,9 @@ const { join } = require('path')
 
 const nodeApi = readFileSync(join(__dirname, '../src/node-api.js')).toString()
 const core = readFileSync(join(__dirname, '../dist/core.js')).toString()
+
 const imports = {}
+const promisified = []
 
 nodeApi.replace(/const ([^=]+) = require\('([^']+)'\)/g, (match, imported, id) => {
   if (imported.startsWith('{')) {
@@ -11,16 +13,18 @@ nodeApi.replace(/const ([^=]+) = require\('([^']+)'\)/g, (match, imported, id) =
   }
   imports[id] = imported
 })
+nodeApi.replace(/(\w+): promisify\(\w+\)/g, (match, api) => promisified.push(api))
 
 writeFileSync(join(__dirname, '../dist/index.js'), [
   '\'use strict\'',
   ...Object.keys(imports).map(module => {
     const imported = imports[module]
     if (Array.isArray(imported)) {
-      return `const {${imported.join(',')}}=require('${module}')`
+      return `const {${imported.map(api => promisified.includes(api) ? `${api}:${api}CB`: api).join(',')}}=require('${module}')`
     }
     return `const ${imported}=require('${module}')`
   }),
+  ...promisified.map(api => `const ${api}=promisify(${api}CB)`),
   core,
   'module.exports=reserve'
 ].join('\n'), { flag: 'w' })
@@ -29,10 +33,11 @@ writeFileSync(join(__dirname, '../dist/index.mjs'), [
   ...Object.keys(imports).map(module => {
     const imported = imports[module]
     if (Array.isArray(imported)) {
-      return `import {${imported.join(',')}} from '${module}'`
+      return `import {${imported.map(api => promisified.includes(api) ? `${api} as ${api}CB`: api).join(',')}} from '${module}'`
     }
     return `import ${imported} from '${module}'`
   }),
+  ...promisified.map(api => `const ${api}=promisify(${api}CB)`),
   core,
   'export default reserve'
 ].join('\n'), { flag: 'w' })
