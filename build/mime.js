c3ebd7a3f054cd42896b06c4d9c3fe52a54a7e6a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-11-10 12:01:46 -0500

    feat(mime): compressed the list to reduce build size

--- build/mime.js
+++ build/mime.js
@@ -1,56 +1,83 @@
 'use strict'
 
-// const { writeFileSync } = require('fs')
+const { writeFileSync } = require('fs')
 const { join } = require('path')
-const zlib = require('zlib')
-const stream = require('stream')
-const util = require('util')
-const pipeline = util.promisify(stream.pipeline)
-
 const mimeTypes = require(join(__dirname, '../src/mime.json'))
 
-const compress = zlib.createBrotliCompress()
+const tokens = [
+  'openxmlformats',
+  'application/',
+  'octet-stream',
+  'presentation',
+  'document',
+  'office',
+  'image/',
+  'audio/',
+  'video/',
+  'text/',
+  'font/',
+  'oasis',
+  'woff',
+  'vnd.',
+  'xml',
+  'zip',
+  'x-'
+]
 
-class Output extends stream.Writable {
-  _write (chunk, encoding, onwrite) {
-    this._buffers.push(chunk)
-    this._length += chunk.length
-    onwrite()
-  }
-
-  constructor () {
-    super()
-    this._length = 0
-    this._buffers = []
-  }
-
-  get length () {
-    return this._length
-  }
-
-  toString () {
-    return Buffer.concat(this._buffers).toString('base64')
-  }
-}
-const output = new Output()
-
-function * genMimeTypes () {
+function * encode () {
   let lastMime
   for (const type in mimeTypes) {
-    const mime = mimeTypes[type]
+    if (lastMime !== undefined) {
+      yield ','
+    }
+    const mime = mimeTypes[type].replace(new RegExp(tokens.join('|'), 'g'), match => String.fromCharCode(65 + tokens.indexOf(match)))
     if (mime !== lastMime) {
       lastMime = mime
-      yield `${type} ${mime}\n`
+      yield `${type}=${mime}`
     } else {
-      yield `${type}\n`
+      yield `${type}`
     }
   }
 }
 
-const input = stream.Readable.from(genMimeTypes())
+const encoded = [...encode()].join('')
+console.log('Compression done', encoded.length, encoded)
 
-pipeline(input, compress, output)
-  .then(() => {
-    console.log('done.')
-    console.log(output.length, output.toString())
+writeFileSync(join(__dirname, '../src/mime.js'), `'use strict'
+
+const mimeTypes = {}
+
+const tokens = '${tokens.join(',')}'.split(',')
+let source = '${encoded}'
+tokens.forEach((token, index) => {
+  source = source.replace(new RegExp(String.fromCharCode(65 + index), 'g'), token)
+})
+let lastMimeType
+source.split(',').forEach(line => {
+  const [type, mimeType] = line.split('=')
+  if (mimeType) {
+    lastMimeType = mimeType
+  }
+  mimeTypes[type] = lastMimeType
+})
+
+module.exports = mimeTypes
+`)
+
+function decode () {
+  const mimeTypes = {}
+  let source = encoded
+  tokens.forEach((token, index) => {
+    source = source.replace(new RegExp(String.fromCharCode(65 + index), 'g'), token)
   })
+  let lastMimeType
+  source.split(',').forEach(line => {
+    const [type, mimeType] = line.split('=')
+    if (mimeType) {
+      lastMimeType = mimeType
+    }
+    mimeTypes[type] = lastMimeType
+  })
+  console.log(mimeTypes)
+}
+decode()
---
+++ src/mime.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mimeTypes = {}
+
+const tokens = 'openxmlformats,application/,octet-stream,presentation,document,office,image/,audio/,video/,text/,font/,oasis,woff,vnd.,xml,zip,x-'.split(',')
+let source = 'txt=Jplain,text,aac=Haac,abw=BQabiword,arc=BC,avi=IQmsvideo,azw=BNamazon.ebook,bin=BC,bmp=Gbmp,bz=BQbP,bz2=BQbP2,csh=BQcsh,css=Jcss,csv=Jcsv,doc=Bmsword,docx=BNA-FE.wordprocessingml.E,eot=BNms-fontobject,epub=Bepub+P,gif=Ggif,htm=Jhtml,html,ico=GQicon,ics=Jcalendar,jar=Bjava-archive,jpeg=Gjpeg,jpg,js=Bjavascript,json=Bjson,mid=Hmidi,midi,mpeg=Impeg,mpkg=BNapple.installer+O,odp=BNL.openE.D,ods=BNL.openE.spreadsheet,odt=BNL.openE.text,oga=Hogg,ogv=Iogg,ogx=Bogg,otf=Kotf,png=Gpng,pdf=Bpdf,ppt=BNms-powerpoint,pptx=BNA-FE.Dml.D,rar=BQrar-compressed,rtf=Brtf,sh=BQsh,svg=Gsvg+O,swf=BQshockwave-flash,tar=BQtar,tif=Gtiff,tiff,ts=Btypescript,ttf=Kttf,vsd=BNvisio,wav=HQwav,weba=Hwebm,webm=Iwebm,webp=Gwebp,woff=KM,woff2=KM2,xhtml=Bxhtml+O,xls=BNms-excel,xlsx=BNA-FE.spreadsheetml.sheet,xml=BO,xul=BNmozilla.xul+O,zip=BP,7z=BQ7z-compressed'
+tokens.forEach((token, index) => {
+  source = source.replace(new RegExp(String.fromCharCode(65 + index), 'g'), token)
+})
+let lastMimeType
+source.split(',').forEach(line => {
+  const [type, mimeType] = line.split('=')
+  if (mimeType) {
+    lastMimeType = mimeType
+  }
+  mimeTypes[type] = lastMimeType
+})
+
+module.exports = mimeTypes
