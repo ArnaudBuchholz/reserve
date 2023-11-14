feb5158166c89a7d36493a1af87048dba592ee60
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-11-14 07:29:51 -0500

    feat(samples): workaround for slow fs

--- samples/static/http-stream.js
+++ samples/static/http-stream.js
@@ -5,19 +5,33 @@ const host = 'localhost'
 const port = 8084
 
 const path = 'www/index.html'
+const delay = 500
+
+function perfStat (filePath) {
+  const now = Date.now()
+  let promise = perfStat.cache[filePath]
+  if (!promise || (now - promise.when) > delay) {
+    promise = new Promise((resolve, reject) => stat(path, (err, fileStat) => err ? reject(err) : resolve(fileStat)))
+    console.log(now)
+    promise.when = now
+    perfStat.cache[filePath] = promise
+  }
+  return promise
+}
+perfStat.cache = {}
 
 const requestListener = function (req, res) {
-  stat(path, (err, fileStat) => {
+  perfStat(path).then(fileStat => {
     res.writeHead(200, {
       'content-type': 'text/html',
       'content-length': fileStat.size
     })
-    stream = createReadStream(path)
+    const stream = createReadStream(path)
     pipeline(stream, res, () => {})
   })
-};
+}
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
   console.log(`Listening on port ${port}`)
-});
+})
