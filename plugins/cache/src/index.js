2014cf9de3e38eba2b5ba0adb772afc72ab6b5fd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-11-24 22:39:13 -0500

    refactor(cache): use cache helper

--- plugins/cache/src/index.js
+++ plugins/cache/src/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const $cache = Symbol('REserve/cache@data')
-const { body, send } = require('reserve')
+const { body, cache, send } = require('reserve')
 
 const handlers = {}
 
 handlers.GET = async ({ cache, redirect, response }) => {
-  const value = cache[redirect]
+  const value = cache.get(redirect)
   if (value === undefined) {
     send(response, undefined, {
       statusCode: 204
@@ -18,23 +18,41 @@ handlers.GET = async ({ cache, redirect, response }) => {
 
 handlers.POST = async ({ cache, redirect, request, response }) => {
   let statusCode
-  if (Object.prototype.hasOwnProperty.call(cache, redirect)) {
+  if (cache.keys().include(redirect)) {
     statusCode = 200
   } else {
     statusCode = 201
   }
-  cache[redirect] = await body(request)
-  send(response, statusCode)
+  cache.set(redirect, await body(request))
+  send(response, undefined, { statusCode })
 }
 
 handlers.DELETE = async ({ cache, redirect, response }) => {
-  delete cache[redirect]
-  send(response, 204)
+  cache.del(redirect)
+  send(response, undefined, { statusCode: 204 })
 }
 
 module.exports = {
+  schema: {
+    ttl: {
+      type: 'number',
+      defaultValue: 0
+    },
+    max: {
+      type: 'number',
+      default: 0
+    },
+    policy: {
+      type: 'string',
+      default: 'lru'
+    }
+  },
   async validate (mapping) {
-    mapping[$cache] = {}
+    mapping[$cache] = cache({
+      ttl: mapping.ttl || undefined,
+      max: mapping.max || undefined,
+      policy: mapping.policy
+    })
   },
   method: Object.keys(handlers),
   async redirect ({ mapping, match, redirect, request, response }) {
