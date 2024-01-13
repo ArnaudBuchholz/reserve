78619cac3b8611039529797697bb646a21c773c0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-13 09:14:47 -0500

    feat(samples): perf example with timers

--- reserve/tests/perf-example.js
+++ reserve/tests/perf-example.js
@@ -1,7 +1,9 @@
 module.exports = async () => {
   return async () => {
     await (1 + 1)
+    // await new Promise(resolve => setTimeout(resolve, 100))
     await (2 + 2)
+    // await new Promise(resolve => setTimeout(resolve, 100))
     await (3 + 3)
   }
 }
