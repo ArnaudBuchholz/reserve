a6348c79ebc65ea8d055215376e3c9b1f2a2874e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-13 08:40:21 -0500

    feat(performance): - improve promises measurement - script must return a setup function

--- reserve/tests/perf-example.js
+++ reserve/tests/perf-example.js
@@ -1,5 +1,7 @@
 module.exports = async () => {
-  await (1 + 1)
-  await (2 + 2)
-  await (3 + 3)
+  return async () => {
+    await (1 + 1)
+    await (2 + 2)
+    await (3 + 3)
+  }
 }
--- reserve/tests/performance.js
+++ reserve/tests/performance.js
@@ -3,139 +3,144 @@
 const { parseArgs } = require('util')
 const v8 = require('v8')
 
-const {
-  values,
-  positionals: { 0: script }
-} = parseArgs({
-  args: process.argv.slice(2),
-  options: {
-    duration: {
-      type: 'string',
-      short: 'd',
-      default: '10s'
+async function main () {
+  const {
+    values,
+    positionals: { 0: script }
+  } = parseArgs({
+    args: process.argv.slice(2),
+    options: {
+      duration: {
+        type: 'string',
+        short: 'd',
+        default: '10s'
+      },
+      frequency: {
+        type: 'string',
+        short: 'f',
+        default: '1000ms'
+      },
+      parallel: {
+        type: 'string',
+        short: 'p',
+        default: '1'
+      },
+      measureMemory: {
+        type: 'boolean',
+        short: 'm',
+        default: true
+      },
+      measureV8Heap: {
+        type: 'boolean',
+        short: 'h',
+        default: false
+      },
+      measurePromises: {
+        type: 'boolean',
+        short: 'q',
+        default: false
+      }
     },
-    frequency: {
-      type: 'string',
-      short: 'f',
-      default: '1000ms'
-    },
-    parallel: {
-      type: 'string',
-      short: 'p',
-      default: '1'
-    },
-    memory: {
-      type: 'boolean',
-      short: 'm',
-      default: true
-    },
-    heap: {
-      type: 'boolean',
-      short: 'h',
-      default: false
-    },
-    promises: {
-      type: 'boolean',
-      short: 'q',
-      default: false
-    }
-  
-  },
-  allowPositionals: true
-})
+    allowPositionals: true
+  })
 
-const parseDelay = value => {
-  const base = parseInt(value, 10)
-  if (value.endsWith('ms')) {
+  const parseDelay = value => {
+    const base = parseInt(value, 10)
+    if (value.endsWith('ms')) {
+      return base
+    }
+    if (value.endsWith('s')) {
+      return base * 1000
+    }
     return base
   }
-  if (value.endsWith('s')) {
-    return base * 1000
+
+  const duration = parseDelay(values.duration)
+  const frequency = parseDelay(values.frequency)
+  const parallel = parseInt(values.parallel, 10)
+  const { measureMemory, measureV8Heap, measurePromises } = values
+  console.log('duration (ms)   ', ':', duration)
+  console.log('frequency (ms)  ', ':', frequency)
+  console.log('parallel        ', ':', parallel)
+  console.log('measure memory  ', ':', measureMemory)
+  console.log('measure v8 heap ', ':', measureV8Heap)
+  console.log('measure promises', ':', measurePromises)
+  console.log('script          ', ':', script)
+
+  const setup = require(script)
+  if (typeof setup !== 'function') {
+    throw new Error('Script must be a function exported with CommonJS')
   }
-  return base
-}
-
-const duration = parseDelay(values.duration)
-const frequency = parseDelay(values.frequency)
-const parallel = parseInt(values.parallel, 10)
-const { memory, heap, promises } = values
-console.log('duration (ms)   ', ':', duration)
-console.log('frequency (ms)  ', ':', frequency)
-console.log('parallel        ', ':', parallel)
-console.log('memory          ', ':', memory)
-console.log('v8 heap         ', ':', heap)
-console.log('pending promises', ':', promises)
-console.log('script          ', ':', script)
-
-let callback
-try {
-  callback = require(script)
-} catch (e) {
-  console.error(e.message)
-  process.exit(-1)
-}
-callback = require(script)
-if (typeof callback !== 'function') {
-  console.error('Script must be a function exported with CommonJS')
-  process.exit(-2)
-}
-
-const TICKS = ['\u280b', '\u2819', '\u2839', '\u2838', '\u283c', '\u2834', '\u2826', '\u2827', '\u2807', '\u280f']
-let tick = -1
-let measureTick = performance.now()
-const measures = []
-const endAfter = measureTick + duration
-let steps = 0
-let stepsCompleted = 0
-let stepsKilled = 0
-let pendingPromisesCount = 0
-
-if (promises) {
-  v8.promiseHooks.onInit(() => ++pendingPromisesCount)
-  v8.promiseHooks.onSettled(() => --pendingPromisesCount)
-}
-
-const measure = (tick = performance.now()) => {
-  const data = {
-    tick,
-    started: steps,
-    completed: stepsCompleted
+  const callback = await setup()
+  if (typeof callback !== 'function') {
+    throw new Error('Script must return a function')
   }
-  if (memory) {
-    data.memory = process.memoryUsage()
+
+  const TICKS = ['\u280b', '\u2819', '\u2839', '\u2838', '\u283c', '\u2834', '\u2826', '\u2827', '\u2807', '\u280f']
+  let tick = -1
+  let measureTick = performance.now()
+  const measures = []
+  const endAfter = measureTick + duration
+  let steps = 0
+  let stepsCompleted = 0
+  let stepsKilled = 0
+  const promises = {
+    init: 0,
+    settled: 0
   }
-  if (heap) {
-    data.heap = v8.getHeapStatistics()
-  }
+
   if (promises) {
-    data.promises = {
-      pending: pendingPromisesCount
+    v8.promiseHooks.onInit(() => ++promises.init)
+    v8.promiseHooks.onSettled(() => ++promises.settled)
+  }
+
+  const measure = (tick = performance.now()) => {
+    const data = {
+      tick,
+      started: steps,
+      completed: stepsCompleted
+    }
+    if (measureMemory) {
+      data.memory = process.memoryUsage()
+    }
+    if (measureV8Heap) {
+      data.v8Heap = v8.getHeapStatistics()
+    }
+    if (measurePromises) {
+      data.promises = { ...promises }
+    }
+    measures.push(data)
+  }
+
+  const step = async () => {
+    const id = ++steps
+    const start = performance.now()
+    await callback()
+    const end = performance.now()
+    ++stepsCompleted
+    if (start >= measureTick) {
+      measure()
+      process.stdout.write(' ' + TICKS[(++tick) % TICKS.length] + ' ' + id.toString() + '\x1b[1G')
+      measureTick = start + frequency
+    }
+    if (end < endAfter) {
+      setImmediate(step)
+      return
+    }
+    if (++stepsKilled === parallel) {
+      measure()
+      console.log('iterations    ', ':', steps)
+      console.log(measures)
     }
   }
-  measures.push(data)
-}
 
-const step = async () => {
-  const id = ++steps
-  const start = performance.now()
-  await callback()
-  const end = performance.now()
-  ++stepsCompleted
-  if (start >= measureTick) {
-    measure()
-    process.stdout.write(' ' + TICKS[(++tick) % TICKS.length] + ' ' + id.toString() + '\x1b[1G')
-    measureTick = start + frequency
-  }
-  if (end < endAfter) {
-    return step()
-  }
-  if (++stepsKilled === parallel) {
-    measure()
-    console.log('iterations    ', ':', steps)
-    console.log(measures)
+  for (let i = 0; i < parallel; ++i) {
+    step()
   }
 }
 
-for (let i = 0; i < parallel; ++i) {
-  step()
-}
+main()
+  .catch(e => {
+    console.error(e.message)
+    process.exit(-1)
+  })
