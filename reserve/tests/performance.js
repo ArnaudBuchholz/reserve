5bfe9817af85c87c61655b2745a80b72ddac350a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-13 09:15:19 -0500

    feat(performance): serialize measures to disk

--- reserve/tests/performance.js
+++ reserve/tests/performance.js
@@ -2,6 +2,8 @@
 
 const { parseArgs } = require('util')
 const v8 = require('v8')
+const { join } = require('path')
+const { writeFileSync } = require('fs')
 
 async function main () {
   const {
@@ -67,7 +69,7 @@ async function main () {
   console.log('measure promises', ':', measurePromises)
   console.log('script          ', ':', script)
 
-  const setup = require(script)
+  const setup = require(join(process.cwd(), script))
   if (typeof setup !== 'function') {
     throw new Error('Script must be a function exported with CommonJS')
   }
@@ -76,12 +78,14 @@ async function main () {
     throw new Error('Script must return a function')
   }
 
+  const jsonl = join(process.cwd(), script + `.${new Date().toISOString().replace(/:|-|T|Z/g, '')}.jsonl`)
+
   const TICKS = ['\u280b', '\u2819', '\u2839', '\u2838', '\u283c', '\u2834', '\u2826', '\u2827', '\u2807', '\u280f']
   let tick = -1
   let measureTick = performance.now()
-  const measures = []
   const endAfter = measureTick + duration
   let steps = 0
+  let stepsTimeSpent = 0
   let stepsCompleted = 0
   let stepsKilled = 0
   const promises = {
@@ -96,7 +100,8 @@ async function main () {
 
   const measure = (tick = performance.now()) => {
     const data = {
-      tick,
+      tick: Math.ceil(tick),
+      timeSpent: stepsTimeSpent,
       started: steps,
       completed: stepsCompleted
     }
@@ -109,7 +114,7 @@ async function main () {
     if (measurePromises) {
       data.promises = { ...promises }
     }
-    measures.push(data)
+    writeFileSync(jsonl, JSON.stringify(data) + '\n', { flag: 'a' })
   }
 
   const step = async () => {
@@ -117,6 +122,7 @@ async function main () {
     const start = performance.now()
     await callback()
     const end = performance.now()
+    stepsTimeSpent += Math.ceil(end - start)
     ++stepsCompleted
     if (start >= measureTick) {
       measure()
@@ -130,10 +136,10 @@ async function main () {
     if (++stepsKilled === parallel) {
       measure()
       console.log('iterations    ', ':', steps)
-      console.log(measures)
     }
   }
 
+  measure()
   for (let i = 0; i < parallel; ++i) {
     step()
   }
