eadf7494738f3bc394290eeb61390c0b57e8fc1b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-13 00:36:42 -0500

    feat(performance): adds memory measurement

--- reserve/tests/performance.js
+++ reserve/tests/performance.js
@@ -22,6 +22,11 @@ const {
       type: 'string',
       short: 'p',
       default: '1'
+    },
+    memory: {
+      type: 'boolean',
+      short: 'm',
+      default: true
     }
   },
   allowPositionals: true
@@ -41,9 +46,11 @@ const parseDelay = value => {
 const duration = parseDelay(values.duration)
 const frequency = parseDelay(values.frequency)
 const parallel = parseInt(values.parallel, 10)
+const { memory } = values
 console.log('duration (ms) ', ':', duration)
 console.log('frequency (ms)', ':', frequency)
 console.log('parallel      ', ':', parallel)
+console.log('memory        ', ':', memory)
 console.log('script        ', ':', script)
 
 let callback
@@ -59,21 +66,48 @@ if (typeof callback !== 'function') {
   process.exit(-2)
 }
 
-const endAfter = performance.now() + duration
-let count = 0
+const TICKS = ['\u280b', '\u2819', '\u2839', '\u2838', '\u283c', '\u2834', '\u2826', '\u2827', '\u2807', '\u280f']
+let tick = -1
+let measureTick = performance.now()
+const measures = []
+const endAfter = measureTick + duration
+let steps = 0
+let stepsCompleted = 0
+let stepsKilled = 0
 
-const task = async () => {
-  const id = ++count
+const measure = (tick = performance.now()) => {
+  const data = {
+    tick,
+    started: steps,
+    completed: stepsCompleted
+  }
+  if (memory) {
+    data.memory = process.memoryUsage()
+  }
+  measures.push(data)
+}
+
+const step = async () => {
+  const id = ++steps
   const start = performance.now()
   await callback()
   const end = performance.now()
-  const ts = end - start
-  console.log(id, ts)
+  ++stepsCompleted
+  if (start >= measureTick) {
+    measure()
+    process.stdout.write(' ' + TICKS[(++tick) % TICKS.length] + ' ' + id.toString() + '\x1b[1G')
+    measureTick = start + frequency
+  }
   if (end < endAfter) {
-    return task()
+    return step()
+  }
+  if (++stepsKilled === parallel) {
+    measure()
+    console.log('iterations    ', ':', steps)
+    console.log(measures)
   }
 }
 
 for (let i = 0; i < parallel; ++i) {
-  task()
+  step()
 }
