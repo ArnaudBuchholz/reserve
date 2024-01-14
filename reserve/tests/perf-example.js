961d2ab2723ec5485bebe3a8fb313411b1cda070
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-14 16:11:20 -0500

    feat(performance): removes useless script

--- reserve/tests/perf-example.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  return async () => {
-    await (1 + 1)
-    // await new Promise(resolve => setTimeout(resolve, 100))
-    await (2 + 2)
-    // await new Promise(resolve => setTimeout(resolve, 100))
-    await (3 + 3)
-  }
-}
