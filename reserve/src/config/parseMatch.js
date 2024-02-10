7a9316dd7e4303af5550a0172947a071fed679b5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-02-10 00:03:57 -0500

    feat(match): parse match and find best fit for regex conversion

--- reserve/src/config/parseMatch.js
+++ reserve/src/config/parseMatch.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const regexpClues = '()^$[]|\\?+*{}'.split('')
+
 module.exports = match => {
   if (match instanceof RegExp) {
     return match
@@ -7,5 +9,16 @@ module.exports = match => {
   if (match && match.re) {
     return new RegExp(match.re, match.flags)
   }
-  
+  if (typeof match !== 'string') {
+    throw new Error('Invalid value for match')
+  }
+  if (regexpClues.some(clue => match.includes(clue))) {
+    if (match.startsWith('/')) {
+      match = '^' + match
+    }
+    return new RegExp(match)
+  }
+  return new RegExp(`^${
+    match.replace(/:(\w+)/g, (_, id) => `(?<${id}>[^/]*)`)
+  }(.*)`)
 }
--- reserve/src/config/parseMatch.spec.js
+++ reserve/src/config/parseMatch.spec.js
@@ -4,6 +4,23 @@ const assert = require('assert')
 const parseMatch = require('./parseMatch')
 
 describe('config/parseMatch', () => {
+  const invalidValues = {
+    false: false,
+    true: true,
+    0: 0,
+    '-1': -1,
+    1: 1,
+    'function () {}': function () {},
+    'Symbol(\'test\')': Symbol('test'),
+    '{}': {}
+  }
+  Object.keys(invalidValues).forEach(label => {
+    const invalidValue = invalidValues[label]
+    it(`rejects invalid values (${label})`, () => {
+      assert.throws(() => parseMatch(invalidValue))
+    })
+  })
+
   it('keeps regular expression', () => {
     const re = /whatever/
     assert.strictEqual(parseMatch(re), re)
@@ -19,5 +36,95 @@ describe('config/parseMatch', () => {
     const re = parseMatch({ re: 'a', flags: 'g' })
     assert.ok(re instanceof RegExp)
     assert.strictEqual('a whatever b'.replace(re, 'o'), 'o whotever b')
+  })
+
+  describe('string definition', () => {
+    describe('looks like a regex', () => {
+      it('contains a capturing group', () => {
+        const re = parseMatch('(.*)')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/(.*)/')
+      })
+
+      it('contains the startsWith operator (anywhere)', () => {
+        const re = parseMatch('^/test')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/^\\/test/')
+      })
+
+      it('contains the endsWith operator (anywhere)', () => {
+        const re = parseMatch('test$')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/test$/')
+      })
+
+      it('contains the character range operator', () => {
+        const re = parseMatch('[a-z]')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/[a-z]/')
+      })
+
+      it('contains the alternate operator', () => {
+        const re = parseMatch('a|b')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/a|b/')
+      })
+
+      it('contains the escape character', () => {
+        const re = parseMatch('\\d')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/\\d/')
+      })
+
+      it('contains the optional operator (?)', () => {
+        const re = parseMatch('a?')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/a?/')
+      })
+
+      it('contains the multiplicity operator (+)', () => {
+        const re = parseMatch('a+')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/a+/')
+      })
+
+      it('contains the multiplicity operator (*)', () => {
+        const re = parseMatch('a*')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/a*/')
+      })
+
+      it('contains the multiplicity operator ({})', () => {
+        const re = parseMatch('a{3,}')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/a{3,}/')
+      })
+
+      it('adds the startWith operator if starting with /', () => {
+        const re = parseMatch('/a*')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/^\\/a*/')
+      })
+    })
+
+    describe('otherwise a pattern', () => {
+      it('converts to regex', () => {
+        const re = parseMatch('/path')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/^\\/path(.*)/')
+      })
+
+      it('handles query parameters (:)', () => {
+        const re = parseMatch('/books/:id')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/^\\/books\\/(?<id>[^/]*)(.*)/')
+      })
+
+      it('handles query parameter (:) in the middle of the url', () => {
+        const re = parseMatch('/books/:id/rentals')
+        assert.ok(re instanceof RegExp)
+        assert.strictEqual(re.toString(), '/^\\/books\\/(?<id>[^/]*)\\/rentals(.*)/')
+      })
+    })
   })
 })
