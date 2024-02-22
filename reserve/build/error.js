e3f763c7c8fc69069a0f5cb58b7bc10ade8ac374
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-02-22 07:37:01 -0500

    feat(bundle): error builder

---
+++ reserve/build/error.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { writeFileSync } = require('fs')
+const { join } = require('path')
+const errors = require(join(__dirname, '../src/errors.json'))
+
+writeFileSync(join(__dirname, '../src/error.js'), `'use strict'
+
+const interpolate = require('./helpers/interpolate')
+
+const errors = '${Object.values(errors).join('|')}'.split('|')
+
+function newError (code, groups) {
+  let message = errors[code]
+  if (groups) {
+    message = interpolate({ groups }, message)
+  }
+  const error = new Error(message)
+  error.name = 'REserveError'
+  error.code = code
+  return error
+}
+
+function throwError (code) {
+  throw newError(code)
+}
+
+module.exports = {
+  throwError,
+  newError,
+  ${
+    Object.keys(errors)
+      .map((name, index) => `${name}: ${index}`)
+      .join(',\n  ')
+  }
+}
+`)
