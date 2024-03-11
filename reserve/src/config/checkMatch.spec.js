0a943b48cca5cccbb82d8b68ade9820c1cf8efcd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-03-11 13:44:40 -0400

    test(checkMatch): first test

--- reserve/src/config/checkMatch.spec.js
+++ reserve/src/config/checkMatch.spec.js
@@ -1,8 +1,13 @@
 'use strict'
-
-'use strict'
 const { describe, it } = require('mocha')
+const assert = require('assert')
+const checkMatch = require('./checkMatch')
+const { $mappingMatch } = require('../symbols')
 
 describe('config/checkMatch', () => {
-
+  it('supports no settings', () => {
+    const mapping = {}
+    checkMatch(mapping)
+    assert.strictEqual(typeof mapping[$mappingMatch], 'function')
+  })
 })
