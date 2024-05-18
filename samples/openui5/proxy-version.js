05f9c6f9b76266e07a956d96a54ceb76f91e0135
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-05-18 00:51:40 -0400

    fix(ui5): update links

--- samples/openui5/proxy-version.js
+++ samples/openui5/proxy-version.js
@@ -1,6 +1,6 @@
 module.exports = async function (request, response, ui5Path) {
   const { referer } = request.headers
-  const version = (/\bversion\b=(\d+\.\d+\.\d+)/.exec(referer) || [0, '1.76.0'])[1]
+  const version = (/\bversion\b=(\d+\.\d+\.\d+)/.exec(referer) || [0, '1.71.68'])[1]
   response.writeHead(302, {
     Location: `/@openui5/${version}/${ui5Path}`
   })
--- samples/openui5/redirect-version.js
+++ samples/openui5/redirect-version.js
@@ -1,8 +1,8 @@
 module.exports = async function (request, response, ui5Path) {
   const { referer } = request.headers
-  const version = (/\bversion\b=(\d+\.\d+\.\d+)/.exec(referer) || [0, '1.76.0'])[1]
+  const version = (/\bversion\b=(\d+\.\d+\.\d+)/.exec(referer) || [0, '1.71.68'])[1]
   response.writeHead(302, {
-    Location: `https://openui5.hana.ondemand.com/${version}/resources/${ui5Path}`
+    Location: `https://ui5.sap.com/${version}/resources/${ui5Path}`
   })
   response.end()
 }
