b5d0e055a706c36987d443ce60d2c5da1c0bf953
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-05-23 08:21:51 -0400

    feat(samples): normalize static server

--- samples/static/fastify.js
+++ samples/static/fastify.js
@@ -6,10 +6,6 @@ fastify.register(require('@fastify/static'), {
   root: join(__dirname, 'www')
 })
 
-fastify.get('/', function (req, reply) {
-  reply.sendFile('index.html')
-})
-
 fastify.get('/hello', function (req, reply) {
   reply.send('Hello World !')
 })
