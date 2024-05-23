aaa092d57f787e4f0b7abf8b958bd12d8cd95c2d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-05-23 08:22:04 -0400

    feat(samples): normalize static server

--- samples/static/http.js
+++ samples/static/http.js
@@ -1,24 +1,39 @@
 const http = require('http')
-const { readFileSync } = require('fs')
+const { createReadStream, stat } = require('fs')
+const { join, extname } = require('path')
 const { NATIVE_PORT: PORT } = require('./ports.js')
+const { pipeline } = require('stream')
+const mime = require('mime')
 
 const host = 'localhost'
 
-const page = readFileSync('www/index.html').toString()
-
-const requestListener = function (req, res) {
-  if (req.url === '/hello') {
-    res.writeHead(200, {
+const requestListener = function (request, response) {
+  const { url } = request
+  if (url === '/hello') {
+    response.writeHead(200, {
       'content-type': 'text/plain',
       'content-length': 13
     })
-    res.end('Hello World !')
+    response.end('Hello World !')
   } else {
-    res.writeHead(200, {
-      'content-type': 'text/html',
-      'content-length': page.length
+    const path = join(__dirname, 'www', url)
+    stat(path, (err, fileStat) => {
+      if (err) {
+        response.writeHead(500)
+        response.end()
+      } else {
+        response.writeHead(200, {
+          'content-type': mime.getType(extname(path)),
+          'content-length': fileStat.size
+        })
+        const stream = createReadStream(path)
+        pipeline(stream, response, err => {
+          if (err) {
+            response.end()
+          }
+        })
+      }
     })
-    res.end(page)
   }
 }
 
