020b6f3ed7775e34c353dc05b499e50bd1471caf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-06-10 08:43:54 -0400

    doc: improve type definition

--- reserve/@types/index.d.ts
+++ reserve/@types/index.d.ts
@@ -3,6 +3,21 @@ import { Server as HttpsServer } from 'https'
 import { Http2Server } from 'http2'
 
 declare module 'reserve' {
+  class Request extends IncomingMessage {
+    /** Sets a forged (unverified) URL for the request */
+    setForgedUrl: (url: string) => void
+  }
+
+  class Response extends ServerResponse {
+    constructor(request?: Request)
+    /** Waits for the response to be completed */
+    waitForFinish: () => Promise<void>
+    /** Checks if the response is still in the initial state */
+    isInitial: () => boolean
+    /** Sets the response' writes asynchronous */
+    setAsynchronous: () => void
+  }
+
   type RedirectResponse = 
     | void
     /** Ends the response with corresponding status code */
@@ -10,25 +25,20 @@ declare module 'reserve' {
     /** Triggers an internal redirect */
     | string
 
-  class Request extends IncomingMessage {
-    setForgedUrl: (url: string) => void
-  }
-
-  class Response extends ServerResponse {
-    constructor(req?: Request)
-    waitForFinish: () => Promise<void>
-    isInitial: () => boolean
-    setAsynchronous: () => void
-  }
-
   type IfMatcher = (request: IncomingMessage, url: string, match: RegExpMatchArray) => boolean | RedirectResponse
 
   interface BaseMapping {
+    /** URL matching, capturing groups are allowed */
     match?: string | RegExp
+    /** Request method matching */
     method?: string
+    /** Inverts the matching process when set to true, enabling the implementation of an 'all but' pattern */
     'invert-match'?: boolean
+    /** Executed only if the mapping matches the request, enabling finer control */
     'if-match'?: IfMatcher
+    /** Ignore any request processed by this mapping when updating the list of mappings */
     'exclude-from-holding-list'?: boolean
+    /** Current working folder */
     cwd?: string
   }
 
@@ -58,82 +68,140 @@ declare module 'reserve' {
   // region file
 
   interface ReadStreamOptions {
+    /** Start position in the file */
     start: number
+    /** End position in the file, inclusive */
     end: number
   }
 
   interface CustomFileSystemStat {
+    /** Returns true if the path is a directory */
     isDirectory: () => boolean
+    /** Returns file size */
     size: number
+    /** Returns file modification time */
     mtime: Date
   }
 
   interface CustomFileSystem {
+    /** Reads directory content */
     readdir: (folderPath: string) => Promise<string[]>
+    /** Gets path stat */
     stat: (filePath: string) => Promise<CustomFileSystemStat>
     createReadStream: (filePath: string, options?: ReadStreamOptions) => Promise<ReadableStream>
   }
 
   interface PunycacheOptions {
+    /** Time to live in the cache, expressed in milliseconds (default: Number.POSITIVE_INFINITY) */
     ttl?: number
+    /** Maximum number of keys to be stored in the cache (default: Number.POSITIVE_INFINITY) */
     max?: number
-    policy?: 'lru' | 'lfu'
+    /** Cache replacement policy (default: lru) */
+    policy?:
+      /** Least Recently Used (each get / set updates used timestamp) */
+      | 'lru'
+      /** Least Frequently Used (each get / set increments usage frequency) */
+      | 'lfu'
   }
 
   interface FileMapping extends BaseMapping {
+    /** Path to the file to be served (may contain capturing groups placeholders such as $1) */
     file: string
+    /** Dictionary indexed by file extension that overrides mime type resolution */
     'mime-types'?: { [key: string]: string }
+    /** Configures caching strategy, see documentation */
     'caching-strategy'?: 'modified' | number
+    /** Custom file system implementation */
     'custom-file-system'?: ExternalModule | CustomFileSystem
+    /** Cache file system information for performance (disabled by default) */
     'static'?: boolean | PunycacheOptions
   }
 
   // endregion file
 
+  type Headers = { [key in string]?: string | string[]}
+
   // region status
 
   interface StatusMapping extends BaseMapping {
+    /** HTTP status code for the response */
     status: number
-    headers?: { [key: string]: string }
+    /** Additional headers */
+    headers?: Headers
   }
 
   // endregion status
 
+  /** REserve configuration information */
+  interface IConfiguration {
+    /** Dictionary of handlers indexed by their prefix */
+    readonly handlers: { [key in string]?: readonly Handler }
+    /** List of active mappings */
+    readonly mappings: Mapping[]
+    readonly protocol: 'http' | 'https'
+    /** HTTP/2 is enabled */
+    readonly http2: boolean
+    /** Validate and update the list of active mappings */
+    setMappings: (mappings: Mapping[], request: IncomingMessage, timeout?: number) => Promise<void>
+    /** Dispatch a request internally */
+    dispatch: (request: IncomingMessage, response: ServerResponse) => Promise<void>
+  }
+
   // region url
 
-  type Headers = { [key: string]: string | string[] }
-
   interface RequestSummary {
+    /** Request method */
     method: string
+    /** Request URL */
     url: string
+    /** Request headers */
     headers: Headers
   }
 
   interface ForwardRequestContext {
+    /** REserve configuration information */
     configuration: IConfiguration
+    /** Placeholder allocated to transmit additional information to the response */
     context: object
+    /** Mapping being executed */
     mapping: UrlMapping
+    /** Current mapping's regular expression match */
     match: RegExpMatchArray
+    /** Request description (may be changed) */
     request: RequestSummary
+    /** Incoming request object */
     incoming: IncomingMessage
   }
 
   interface ForwardResponseContext {
+    /** REserve configuration information */
     configuration: IConfiguration
+    /** Placeholder allocated to transmit additional information to the response */
     context: object
+    /** Mapping being executed */
     mapping: UrlMapping
+    /** Current mapping's regular expression match */
     match: RegExpMatchArray
+    /** Request description */
     request: RequestSummary
+    /** Response status code */
     statusCode: number
+    /** Response headers */
     headers: Headers
   }
 
   interface UrlMapping extends BaseMapping {
+    /** URL to redirect to (may contain capturing groups placeholders such as $1) */
     url: string
+    /** Convert secure cookies to unsecure one (useful when switching between http and https) */
     'unsecure-cookies'?: boolean
+    /** Hook before forwarding the request */
     'forward-request'?: ExternalModule | ((context: ForwardRequestContext) => Promise<void>)
+    /** Hook before forwarding the response */
     'forward-response'?: ExternalModule | ((context: ForwardResponseContext) => Promise<RedirectResponse>)
+    /** Ignore unverifiable SSL certificates */
     'ignore-unverifiable-certificate'?: boolean
+    /** Converts 'location' header from relative to absolute */
     'absolute-location'?: boolean
   }
 
@@ -150,47 +218,58 @@ declare module 'reserve' {
 
   // region helpers
 
+  /** Listen to server events and log them */
   function log (server: Server, verbose?: boolean): Server
 
+  /** Interpolation helper */
   function interpolate (match: RegExpMatchArray, pattern: string): string
   function interpolate (match: RegExpMatchArray, pattern: object): object
 
   interface BodyOptions {
+    /** Ignores response's Content-Length header */
     ignoreContentLength?: boolean
   }
 
   type BodyResult = Promise<Buffer | string | object> & {
+    /** Returns the body as a Buffer */
     buffer: () => Promise<Buffer>
+    /** Returns the body as a string */
     text: () => Promise<string>
+    /** Returns the body as a parsed JSON */
     json: () => Promise<any>
   }
 
+  /** Body deserialization helper */
   function body (request: IncomingMessage, options?: BodyOptions): BodyResult
 
   interface SendOptions {
-    statusCode?: number /* defaulted to 200 */
+    /** Status code (defaulted to 200) */
+    statusCode?: number
+    /** Header (might override 'content-type' and 'content-length') */
     headers?: Headers
-    noBody?: boolean /* do not send body */
+    /** Do not send body (only headers) */
+    noBody?: boolean
   }
 
+  /** Response sending helper */
   function send (response: ServerResponse, data: ReadableStream, options?: SendOptions): Promise<void>
   function send (response: ServerResponse, data?: string | object, options?: SendOptions): void
 
+  /** Response capturing helper (check documentation) */
   function capture (response: ServerResponse, stream: WritableStream): Promise<void>
 
-  interface PunycacheOptions {
-    ttl?: number
-    max?: number
-    policy?: 'lru' | 'lfu'
-  }
-
   interface PunycacheCache {
+    /** Sets the value in the cache */
     set (key: string, value: any): void
+    /** Gets a value from the cache */
     get (key: string): any
+    /** Deletes a value from the cache */
     del (key: string): void
+    /** Returns the list of keys in the cache */
     keys (): string[]
   }
 
+  /** Cache factory */
   function punycache (options?: PunycacheOptions): PunycacheCache
 
   // endregion helpers
@@ -244,15 +323,6 @@ declare module 'reserve' {
   function read (filename: string): Promise<Configuration>
 
   function check (configuration: Configuration): Promise<Configuration>
-
-  interface IConfiguration {
-    readonly handlers: { [key: string]: Handler }
-    readonly mappings: Mapping[]
-    readonly http2: boolean
-    readonly protocol: string
-    setMappings: (mappings: Mapping[], request: IncomingMessage, timeout?: number) => Promise<void>
-    dispatch: (request: IncomingMessage, response: ServerResponse) => Promise<void>
-  }
 
   type ServerEventName = 
     | 'created'
@@ -336,6 +406,11 @@ declare module 'reserve' {
                   : ServerEventCreated | ServerEventReady | ServerEventIncoming | ServerEventError | ServerEventRedirecting | ServerEventRedirected | ServerEventAborted | ServerEventClosed
 
   type ServerListener<EventName = unknown> = (event: ServerEvent<EventName>) => void
+
+  type ServerCloseOptions = {
+    timeout?: number // in milliseconds, default is 5000
+    force?: boolean // if true, closes immediately without waiting for ongoing requests to finish
+  }
 
   interface Server {
     on: <ServerEventName extends string>(eventName: ServerEventName, listener: ServerListener<ServerEventName>) => Server
